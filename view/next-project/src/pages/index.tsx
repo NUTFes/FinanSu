1b143fed790fc800b048370190deec4264dc7d9b
YushiroDodo63 <yushiro0603@gmail.com>
2021-11-16 23:37:33 +0900

    [fix]install chakra ui

---
+++ view/next-project/src/pages/index.tsx
@@ -0,0 +1,10 @@
+import { Box, chakra } from '@chakra-ui/react';
+import React from 'react';
+
+export default function HomePage() {
+  return (
+    <Box>
+      <chakra.h1 color='tomato'>Hello World!</chakra.h1>
+    </Box>
+  );
+}
