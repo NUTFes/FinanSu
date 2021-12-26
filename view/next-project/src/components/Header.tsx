15d76ffaca25b74f1ade58b2191b44e0bd47e065
tanaka-hina <t.hina145.amaou0141@gmail.com>
2021-12-26 23:44:37 +0900

    [mod]Header編集(#43)

--- view/next-project/src/components/Header.tsx
+++ view/next-project/src/components/Header.tsx
@@ -1,9 +1,65 @@
-function Header() {
+import React from "react";
+import {
+  Box,
+  Flex,
+  Text,
+  ChakraProvider
+} from "@chakra-ui/react";
+import { ChevronDownIcon } from "@chakra-ui/icons";
+import { Avatar } from '@chakra-ui/react'
+import theme from "@assets/theme";
+
+const Header = () => {
   return (
-    <header>
-      <h3>FinanSu</h3>
-    </header>
+    <ChakraProvider theme={theme}> 
+      <Flex
+        as="nav"
+        align="center"
+        wrap="wrap"
+        padding="0"
+        color="white"
+        flexDirection="row"
+        width="100%"
+      >
+        <Flex 
+          justify="center"
+          bg="base.1"
+          align="center"
+          width="200px"
+          height="60px" 
+          p="4"
+        >
+          <Text 
+            color="white" 
+            fontSize="24px" 
+            textShadow="4px 2px 0px rgba(0, 0, 0, 0.66)"
+            letterSpacing={"tighter"}
+          >
+            FinanSu
+          </Text>
+        </Flex>
+          <Flex
+            align="center"
+            justify="end"
+            bgGradient="linear(to-br, primary.1, primary.2)"
+            height="60px"
+            flexGrow="1"
+          >
+            <Box>
+              <Avatar size="xs" />
+            </Box>
+            <Box>
+              <Text mx="3">
+              小林諒大
+              </Text>
+            </Box>
+            <Box marginRight="5">
+              <ChevronDownIcon />
+            </Box>
+          </Flex>
+      </Flex>
+    </ChakraProvider>
   );
-}
+};
 
 export default Header;
