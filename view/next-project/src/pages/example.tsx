a1268074b16b9e6cbbff2116efa19a6fdba1eeb1
YushiroDodo63 <yushiro0603@gmail.com>
2022-01-17 23:54:38 +0900

    [fix]fetch method(#71)

--- view/next-project/src/pages/example.tsx
+++ view/next-project/src/pages/example.tsx
@@ -37,47 +37,47 @@ export async function getStaticProps() {
   };
 }
 
-// export async function postBudget() {
-//   const postUrl = 'http://localhost:1323/budget';
-//   const postData = { title: 'Rust勉強', content: '未定', homework: '未定', skill_id: 1 };
-//   const getRes = await get(postUrl);
-//   console.log(getRes.slice(-1)[0].id);
-//   const postReq = await post(postUrl, postData);
-// }
+export async function postBudget() {
+  const postUrl = 'http://localhost:1323/budgets';
+  const postData = { price: 100000, year_id: 1, source_id: 1 };
+  const getRes = await get(postUrl);
+  console.log(getRes.slice(-1)[0].id);
+  const postReq = await post(postUrl, postData);
+}
 
-// export async function putBudget() {
-//   const Url = 'http://localhost:1323/budget';
-//   const getRes = await get(Url);
-//   const putUrl = Url + '/' + getRes.slice(-1)[0].id;
-//   const putData = {
-//     title: 'Rust勉強会',
-//     content: 'Rustとwasbによるフロントエンド実装',
-//     homework: 'ソート',
-//     skill_id: 1,
-//   };
-//   console.log(getRes.slice(-1)[0].id);
-//   const putReq = await put(putUrl, putData);
-// }
+export async function putBudget() {
+  const Url = 'http://localhost:1323/budgets';
+  const getRes = await get(Url);
+  console.log(getRes);
+  const putUrl = Url + '/' + getRes.slice(-1)[0].id;
+  console.log(putUrl);
+  const putData = { price: 2000, year_id: 2, source_id: 2 };
+  console.log(putData);
+  console.log(getRes.slice(-1)[0].id);
+  const putReq = await put(putUrl, putData);
+}
 
-// export async function deleteBudget() {
-//   const Url = 'http://localhost:1323/budget';
-//   const getRes = await get(Url);
-//   const delUrl = Url + '/' + getRes.slice(-1)[0].id;
-//   console.log(getRes.slice(-1)[0].id);
-//   const delReq = await del(delUrl);
-// }
+export async function deleteBudget() {
+  const Url = 'http://localhost:1323/budgets';
+  const getRes = await get(Url);
+  const delUrl = Url + '/' + getRes.slice(-1)[0].id;
+  console.log(getRes.slice(-1)[0].id);
+  const delReq = await del(delUrl);
+}
 
 export default function Example(props: Props) {
   return (
     <div>
-      {/* <Button onClick={postBudget}>POST</Button> */}
-      {/* <Button onClick={putBudget}>PUT</Button> */}
-      {/* <Button onClick={deleteBudget}>DELETE</Button> */}
+      <Button onClick={postBudget}>POST</Button>
+      <Button onClick={putBudget}>PUT</Button>
+      <Button onClick={deleteBudget}>DELETE</Button>
       <Table>
         {props.budget.map((budget) => (
           <Tr key={budget.toString()}>
             <Td>{budget.id}</Td>
             <Td>{budget.price}</Td>
+            <Td>{budget.year_id}</Td>
+            <Td>{budget.source_id}</Td>
           </Tr>
         ))}
       </Table>
