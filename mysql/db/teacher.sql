5ea9d61e85ef675a855ab00a5f0c0d917a7367c7
ryuseifujisaki <fujiryu1122@gmail.com>
2022-01-28 17:07:33 +0900

    [create] DB Add teacher.sql

--- mysql/db/teacher.sql
+++ mysql/db/teacher.sql
@@ -1,12 +1,14 @@
 use finansu_db;
 
-CREATE TABLE users (
+CREATE TABLE teacher(
   id int(10) unsigned not null auto_increment,
   name varchar(255) not null,
-  department_id int(10) ,
+  position varchar(255) not null,
+  department_id int(10),
+  room varchar(255),
   created_at datetime not null default current_timestamp,
   updated_at datetime not null default current_timestamp on update current_timestamp,
   PRIMARY KEY (id)
 );
 
-INSERT into users (name, department_id) values ('技大太郎', 1);
+INSERT into teacher (name,position,department_id,room) values ('test-name','test-position', 1,'605');
