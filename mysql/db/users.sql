8750fddaeaa348df35fa56bfe588f9c5ae2b08eb
ryuseifujisaki <fujiryu1122@gmail.com>
2022-01-28 17:44:36 +0900

    [Fix] user.sql

--- mysql/db/users.sql
+++ mysql/db/users.sql
@@ -3,7 +3,7 @@ use finansu_db;
 CREATE TABLE users (
   id int(10) unsigned not null auto_increment,
   name varchar(255) not null,
-  department_id int(10) ,
+  department_id int(10) not null,
   created_at datetime not null default current_timestamp,
   updated_at datetime not null default current_timestamp on update current_timestamp,
   PRIMARY KEY (id)
