80e653765dce260576f66f261803e4d7a65a9986
sasadai <d.sasaki.nutfes@gmail.com>
2022-02-10 02:17:14 +0900

    [mod]モーダルの作成

---
+++ view/next-project/src/components/General/RegistModal.tsx
@@ -0,0 +1,83 @@
+import {
+  ChakraProvider,
+  Button,
+  Center,
+  Text,
+  Input,
+  Flex,
+  Box,
+  Spacer,
+  VStack,
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalFooter,
+  ModalBody,
+} from '@chakra-ui/react';
+import * as React from 'react';
+import theme from '@assets/theme';
+import { RiCloseCircleLine } from 'react-icons/ri';
+
+const RegistModal = (props) => {
+  const closeModal = () => {
+    props.setShowModal(false);
+  };
+
+  return (
+    <ChakraProvider theme={theme}>
+      <Modal isOpen={props.openModal} onClose={closeModal} isCentered>
+        <ModalOverlay />
+        <ModalContent pb='5' borderRadius='3xl'>
+          <ModalBody p='3'>
+            <Flex mt='5'>
+              <Spacer />
+              <Box mr='5' _hover={{ background: '#E2E8F0', cursor: 'pointer' }}>
+                <RiCloseCircleLine size={'23px'} color={'gray'} onClick={closeModal} />
+              </Box>
+            </Flex>
+            <VStack spacing='30px'>
+              <Text fontSize='xl' color='black.600'>
+                予算の登録
+              </Text>
+              <VStack spacing='15px'>
+                <Flex>
+                  <Center color='black.600' mr='3'>
+                    年度
+                  </Center>
+                  <Input w='100' borderRadius='full' borderColor='primary.1' />
+                </Flex>
+                <Flex>
+                  <Center color='black.600' mr='3'>
+                    項目
+                  </Center>
+                  <Input w='100' borderRadius='full' borderColor='primary.1' />
+                </Flex>
+                <Flex>
+                  <Center color='black.600' mr='3'>
+                    金額
+                  </Center>
+                  <Input w='100' borderRadius='full' borderColor='primary.1' />
+                </Flex>
+              </VStack>
+            </VStack>
+          </ModalBody>
+          <Center>
+            <ModalFooter mt='5' mb='10'>
+              <Button color='white' w='200px' bgGradient='linear(to-br, primary.1, primary.2)'>
+                登録する
+              </Button>
+            </ModalFooter>
+          </Center>
+        </ModalContent>
+      </Modal>
+    </ChakraProvider>
+  );
+};
+
+export default RegistModal;
+
+// class RegistModal extends React.Component {
+//   render() {
+//     return <ChakraProvider theme={theme}></ChakraProvider>;
+//   }
+// }
