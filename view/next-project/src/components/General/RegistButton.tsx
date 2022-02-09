ac7d20e6e187d1ae0f7b1ea2bba0077141e2c0f2
sasadai <d.sasaki.nutfes@gmail.com>
2022-02-10 02:17:14 +0900

    [mod]モーダルの作成

--- view/next-project/src/components/General/RegistButton.tsx
+++ view/next-project/src/components/General/RegistButton.tsx
@@ -1,6 +1,8 @@
 import { ChakraProvider, Button } from '@chakra-ui/react';
 import theme from '@assets/theme';
 import * as React from 'react';
+import RegistModal from './RegistModal';
+import { useState } from 'react';
 
 interface Props {
   width?: string;
@@ -9,15 +11,22 @@ interface Props {
 }
 
 const RegistButton: React.FC<Props> = ({ children, width, height }) => {
+  const [showModal, setShowModal] = useState(false);
+  const ShowModal = () => {
+    setShowModal(true);
+  };
+  //const { isOpen, onOpen, onClose } = useDisclosure();
   return (
     <ChakraProvider theme={theme}>
       <Button
         color='white'
         bgGradient='linear(to-br, primary.1, primary.2)'
         style={{ height, width }}
+        onClick={ShowModal}
       >
         {children}
       </Button>
+      <RegistModal openModal={showModal} setShowModal={setShowModal} />
     </ChakraProvider>
   );
 };
