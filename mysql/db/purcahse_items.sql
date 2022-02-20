2e82c4720f97e54a62d2ab0d6317c7f2fcdf73bd
ryuseifujisaki <fujiryu1122@gmail.com>
2022-02-21 03:02:46 +0900

    [finish] Create purchase_id teble

---
+++ mysql/db/purcahse_items.sql
@@ -0,0 +1,15 @@
+use finansu_db;
+
+CREATE TABLE purchase_items (
+  id int(10) unsigned not null auto_increment,
+  item varchar(255),
+  quantity int(10),
+  detail varchar(255),
+  url varchar(255),
+  purchase_order_id int(10),
+  created_at datetime not null default current_timestamp,
+  updated_at datetime not null default current_timestamp on update current_timestamp,
+  PRIMARY KEY (id)
+);
+
+INSERT into purchase_items (item, quantity, detail, url, purchase_order_id )values ('test-item1', 1, 'test-detail', 'https://nutfes.net', 1);
