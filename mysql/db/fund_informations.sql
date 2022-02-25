b3f2bb22d95a1838ad50658158218f26fc7f59ae
ryuseifujisaki <fujiryu1122@gmail.com>
2022-02-26 01:17:50 +0900

    [Edit] fund_informations detail change remark

--- mysql/db/fund_informations.sql
+++ mysql/db/fund_informations.sql
@@ -7,7 +7,7 @@ CREATE TABLE fund_informations (
   fund_date varchar(255) not null,
   fund_time varchar(255) not null,
   price int(10) not null,
-  detail varchar(255) not null ,
+  remark varchar(255) not null ,
   report_person varchar(255) ,
   report_price int(10) ,
   created_at datetime not null default current_timestamp,
@@ -15,4 +15,4 @@ CREATE TABLE fund_informations (
   PRIMARY KEY (id)
 );
 
-INSERT into fund_informations (contact_person, fund_date, fund_time, price, detail, report_person, report_price) values ('test-person','20220128','16:30',1000,'test-detail','test-report_person',1000);
+INSERT into fund_informations (contact_person, fund_date, fund_time, price, remark, report_person, report_price) values ('test-person','20220128','16:30',1000,'test-remark','test-report_person',1000);
