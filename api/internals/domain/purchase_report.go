0d5f2e34574eb1d55b4bdccf9740baa52710b3ac
ryuseifujisaki <fujiryu1122@gmail.com>
2022-03-14 10:39:26 +0900

    [create] api/internals/domain/purchase_report.go

---
+++ api/internals/domain/purchase_report.go
@@ -0,0 +1,15 @@
+package domain
+
+import (
+	"time"
+)
+
+type PurchaseReport struct {
+	ID   						ID 	`json:"id"`
+	Item 						Item `json:"item"`
+	Price 					Price `json:"price"`
+	UserID 					ID `json:"user_id"`
+	PurchaseOrderID PurchaseOrderID 'json:"purchase_order_id'
+	CreatedAt 			time.Time `json:"created_at"`
+	UpdatedAt				time.Time `json:"updated_at"`
+}
