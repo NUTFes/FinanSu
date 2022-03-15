51a3250b611d84c9d67e40b416c72bf19160bc85
YushiroDodo63 <yushiro0603@gmail.com>
2022-03-15 15:53:09 +0900

    [mod]SSRのfetch時のURLを修正

--- view/next-project/next.config.js
+++ view/next-project/next.config.js
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === 'production';
+
 module.exports = {
   reactStrictMode: true,
+  env: {
+    SSR_API_URI: isProd ? 'http://nutfes-finansu-api:1323' : 'http://nutfes-finansu-api:1323',
+    CSR_API_URI: isProd ? 'http://localhost:1323' : 'http://localhost:1323',
+  },
 };
