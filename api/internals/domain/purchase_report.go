2bdd1c893c6fb77cc10aca745d7aa9d3437e478e
ryuseifujisaki <fujiryu1122@gmail.com>
2022-03-17 15:26:43 +0900

    [FINISH] Edit purchasereports column

--- api/internals/domain/purchase_report.go
+++ api/internals/domain/purchase_report.go
@@ -6,10 +6,8 @@ import (
 
 type PurchaseReport struct {
 	ID   						ID 	`json:"id"`
-	Item 						Item `json:"item"`
-	Price 					Price `json:"price"`
 	UserID 					ID `json:"user_id"`
-	PurchaseOrderID PurchaseOrderID `json:"purchase_order_id`
+	PurchaseOrderID PurchaseOrderID `json:"purchase_order_id"`
 	CreatedAt 			time.Time `json:"created_at"`
 	UpdatedAt				time.Time `json:"updated_at"`
 }
