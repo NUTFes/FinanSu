4036de56b9121967ad7839555d7bad0078828da8
ryuseifujisaki <fujiryu1122@gmail.com>
2022-04-03 16:19:38 +0900

    [FIX] fund_information API teacherの全情報取得

--- api/internals/domain/fund_information.go
+++ api/internals/domain/fund_information.go
@@ -18,10 +18,15 @@ type FundInformation struct {
 
 type FundInforWithUserAndTeacher struct {
 	ID           ID        `json:"id"`
-	UName         string   `json:"user_name"`
-	TName         string   `json:"teacher_name"`
+	UName        string    `json:"user_name"`
+	TName        string    `json:"teacher_name"`
+	Position		 string    `json:"teacher_position"`
+	DName				 string    `json:"department_name"`
+	Room				 string		 `json:"teacher_room"`
+	IsBlack			 bool			 `json:"teacher_is_black"`
+	TRemark			 string		 `json:"teacher_remark"`
 	Price        Price     `json:"price"`
-	Remark       string    `json:"remark"`
+	FRemark			 string		 `json:"fund_information_remark"`
 	IsFirstCheck bool      `json:"is_first_check"`
 	IsLastCheck  bool      `json:"is_last_check"`
 	CreatedAt    time.Time `json:"created_at"`
