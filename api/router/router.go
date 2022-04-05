129b576faf912003e66486f64a47048422acb473
ryuseifujisaki <fujiryu1122@gmail.com>
2022-04-05 16:10:06 +0900

    [FINISH] Create-API-FundInformationBiID

--- api/router/router.go
+++ api/router/router.go
@@ -121,6 +121,7 @@ func (r router) ProvideRouter(e *echo.Echo) {
 	e.PUT("/fund_informations/:id", r.fundInformationController.UpdateFundInformation)
 	e.DELETE("/fund_informations/:id", r.fundInformationController.DestroyFundInformation)
 	e.GET("/get_fund_informations_for_view", r.fundInformationController.IndexFundInforUserAndTeach)
+	e.GET("/get_fund_informations_for_view/:id", r.fundInformationController.ShowFundInforUserAndTeach)
 
 	// parcahseordersのRoute
 	e.GET("/purchaseorders", r.purchaseOrderController.IndexPurchaseOrder)
