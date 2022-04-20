da32f95a66209dba9ba5862dbf472d83f1ac7776
YushiroDodo63 <yushiro0603@gmail.com>
2022-04-20 17:52:08 +0900

    [mod]新規登録時に学科を選択するよう修正(#32)

--- view/next-project/src/components/auth/SignUpView.tsx
+++ view/next-project/src/components/auth/SignUpView.tsx
@@ -3,18 +3,18 @@ import Router from 'next/router';
 import { signUp } from '@api/signUp';
 import { get, post } from '@api/user';
 import SubmitButton from '@components/General/RegistButton';
-import { ChakraProvider, Center, Input, Box, Heading } from '@chakra-ui/react';
+import { ChakraProvider, Center, Box, Heading, Input, Select } from '@chakra-ui/react';
 import theme from '@assets/theme';
 
-interface postData {
+interface PostData {
   email: string;
   password: string;
   passwordConfirmation: string;
 }
 
 interface User {
-  name: string;
-  department_id: number;
+  userName: string;
+  departmentId: number;
 }
 
 interface Department {
@@ -25,25 +25,10 @@ interface Department {
 }
 
 interface Props {
-  department: Department[];
+  departments: Department[];
 }
 
-export async function getServerSideProps() {
-  const getDepartmentsUrl: string = process.env.SSR_API_URI + '/departments';
-  const departmentsRes: Department = await get(getDepartmentsUrl);
-  return {
-    props: {
-      departments: departmentsRes,
-    },
-  };
-}
-
-export const postUser = async (data: postData) => {
-  const postUserData: User = {
-    name: '',
-    department_id: 1,
-  };
-
+export const postUser = async (data: PostData, postUserData: User) => {
   const getUrl: string = process.env.CSR_API_URI + '/users';
   const postUserUrl: string = process.env.CSR_API_URI + '/users';
   const signUpUrl: string = process.env.CSR_API_URI + '/mail_auth/signup';
@@ -64,51 +49,84 @@ export const postUser = async (data: postData) => {
 };
 
 export default function SignUpView(props: Props) {
-  const [formData, setFormData] = useState({
+  const [postUserData, setPostUserData] = useState({
     userName: '',
+    departmentId: 1,
+  });
+  const [formData, setFormData] = useState({
     email: '',
     password: '',
     passwordConfirmation: '',
   });
-  const handler = (input: string) => (e: React.ChangeEvent<HTMLInputElement>) => {
-    setFormData({ ...formData, [input]: e.target.value });
-  };
-
+  const userDataHandler =
+    (input: string) =>
+    (e: React.ChangeEvent<HTMLSelectElement> | React.ChangeEvent<HTMLInputElement>) => {
+      setPostUserData({ ...postUserData, [input]: e.target.value });
+    };
+  const formDataHandler =
+    (input: string) =>
+    (e: React.ChangeEvent<HTMLSelectElement> | React.ChangeEvent<HTMLInputElement>) => {
+      setFormData({ ...formData, [input]: e.target.value });
+    };
   return (
     <ChakraProvider theme={theme}>
       <Center>
         <Box p='5' mb='2'>
-          <Box my='2'>
+          <Box my='3'>
             <Heading as='h4' size='md' my='2'>
               Name
             </Heading>
-            <Input type='text' value={formData.userName} onChange={handler('userName')} />
+            <Input
+              type='text'
+              value={postUserData.userName}
+              onChange={userDataHandler('userName')}
+            />
             <p>例: 技大太郎</p>
           </Box>
-          <Box my='2'>
+          <Box my='3'>
+            <Heading as='h4' size='md' my='2'>
+              学科
+            </Heading>
+            <Select
+              value={postUserData.departmentId}
+              onChange={userDataHandler('departmentId')}
+              borderRadius='full'
+              borderColor='primary.1'
+              w='224px'
+            >
+              {props.departments.map((department) => (
+                <option value={department.id}>{department.name}</option>
+              ))}
+            </Select>
+          </Box>
+          <Box my='3'>
             <Heading as='h4' size='md' my='2'>
               Email
             </Heading>
-            <Input type='text' value={formData.email} onChange={handler('email')} />
+            <Input type='text' value={formData.email} onChange={formDataHandler('email')} />
             <p>例: gidai-taro@email.com</p>
           </Box>
-          <Box my='2'>
+          <Box my='3'>
             <Heading as='h4' size='md' my='2'>
               Password
             </Heading>
-            <Input type='password' value={formData.password} onChange={handler('password')} />
+            <Input
+              type='password'
+              value={formData.password}
+              onChange={formDataHandler('password')}
+            />
           </Box>
-          <Box my='2'>
+          <Box my='3'>
             <Heading as='h4' size='md' my='2'>
               Password Confirmation
             </Heading>
             <Input
               type='password'
               value={formData.passwordConfirmation}
-              onChange={handler('passwordConfirmation')}
+              onChange={formDataHandler('passwordConfirmation')}
             />
           </Box>
-          <SubmitButton onClick={() => postUser(formData)}>Next</SubmitButton>
+          <SubmitButton onClick={() => postUser(formData, postUserData)}>Next</SubmitButton>
         </Box>
       </Center>
     </ChakraProvider>
