98bbf93f8515b872f1459fc2b41621883d83488e
YushiroDodo63 <yushiro0603@gmail.com>
2022-04-20 17:14:03 +0900

    [add]ユーザの新規登録の追加(#32)

--- view/next-project/src/pages/index.tsx
+++ view/next-project/src/pages/index.tsx
@@ -1,10 +1,73 @@
-import { Box, chakra } from '@chakra-ui/react';
-import React from 'react';
+import { useState } from 'react';
+import LoginLayout from '@components/layout/LoginLayout';
+import LoginView from '@components/auth/LoginView';
+import SignUpView from '@components/auth/SignUpView';
+import type { NextPage } from 'next';
+import { ChakraProvider, Center, Flex, Box, Heading, Link, Spacer } from '@chakra-ui/react';
+import theme from '@assets/theme';
 
-export default function HomePage() {
+const Home: NextPage = () => {
+  let [isMember, setIsMember] = useState(true);
+  const cardContent = (isMember: boolean) => {
+    if (isMember) {
+      return (
+        <>
+          <Flex justify='center' align='center' mt='1.5rem'>
+            <Center>
+              <Link onClick={() => setIsMember(!isMember)}>
+                <Heading as='h3' size='md'>
+                  Log in
+                </Heading>
+              </Link>
+            </Center>
+            <Center mx='1rem'>
+              <Heading as='h4' size='md'>
+                /
+              </Heading>
+            </Center>
+            <Center gap='2rem'>
+              <Link onClick={() => setIsMember(!isMember)}>Sign up</Link>
+            </Center>
+          </Flex>
+          {/* <LoginView /> */}
+        </>
+      );
+    } else {
+      return (
+        <>
+          <Flex justify='center' align='center' mt='1.5rem'>
+            <Center>
+              <Link onClick={() => setIsMember(!isMember)}>
+                <Heading as='h3' size='md'>
+                  Sign up
+                </Heading>
+              </Link>
+            </Center>
+            <Center mx='1rem'>
+              <Heading as='h4' size='md'>
+                /
+              </Heading>
+            </Center>
+            <Center gap='2rem'>
+              <Link onClick={() => setIsMember(!isMember)}>Log in</Link>
+            </Center>
+          </Flex>
+          <SignUpView />
+        </>
+      );
+    }
+  };
   return (
-    <Box>
-      <chakra.h1 color='tomato'>Hello World!</chakra.h1>
-    </Box>
+    <LoginLayout>
+      <ChakraProvider theme={theme}>
+        <Center>
+          <Box m='2rem' px='10' boxShadow='base' rounded='lg'>
+            <Box gap='gap-s'>{cardContent(isMember)}</Box>
+          </Box>
+        </Center>
+      </ChakraProvider>
+    </LoginLayout>
   );
-}
+};
+
+export default Home;
---
+++ view/next-project/src/utils/api/signUp.ts
@@ -0,0 +1,60 @@
+export const get = async (url: string) => {
+  const res = await fetch(url, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
+  return await res.json();
+};
+
+export const get_with_token = async (url: string) => {
+  const res = await fetch(url, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+      'access-token': localStorage.getItem('access-token') || 'none',
+      client: localStorage.getItem('client') || 'none',
+      uid: localStorage.getItem('uid') || 'none',
+    },
+  });
+  return await res.json();
+};
+
+export const signUp = async (url: string, data: any, userID: number) => {
+  const email = data.email;
+  const password = data.password;
+  const postUrl = url + '?email=' + email + '&password=' + password + '&user_id=' + userID;
+  const res = await fetch(postUrl, {
+    method: 'POST',
+    mode: 'cors',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(data),
+  });
+  return await res;
+  // return await res.json();
+};
+
+export const put = async (url: string, data: any) => {
+  const price = data.price;
+  const year_id = data.year_id;
+  const source_id = data.source_id;
+  const putUrl = url + '?price=' + price + '&year_id=' + year_id + '&source_id=' + source_id;
+  console.log(putUrl, data.price, data.year_id, data.source_id);
+  const res = await fetch(putUrl, {
+    method: 'PUT',
+    mode: 'cors',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(data),
+  });
+  return await res.json();
+};
+
+export const del = async (url: string) => {
+  const res = await fetch(url, { method: 'DELETE' });
+  return await res.json();
+};
