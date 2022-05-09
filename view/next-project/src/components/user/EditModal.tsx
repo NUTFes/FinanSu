d394f0405948545da5fb3b7d54d5e13c02f72ae4
YushiroDodo63 <yushiro0603@gmail.com>
2022-05-09 14:06:10 +0900

    [fix]npm run build error(#251)

--- view/next-project/src/components/user/EditModal.tsx
+++ view/next-project/src/components/user/EditModal.tsx
@@ -132,7 +132,9 @@ export default function FundInformationEditModal(props: ModalProps) {
                       w='224px'
                     >
                       {props.departments.map((data) => (
-                        <option value={data.id}>{data.name}</option>
+                        <option key={data.id} value={data.id}>
+                          {data.name}
+                        </option>
                       ))}
                     </Select>
                   </GridItem>
