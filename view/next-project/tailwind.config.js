e9c953d137fd02498fdc15cafba3acfd7b7219c5
YushiroDodo63 <yushiro0603@gmail.com>
2022-07-17 15:06:32 +0900

    [add]tailwind css を追加

---
+++ view/next-project/tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,ts,jsx,tsx}"], 
+  theme: {
+    colors: {
+      'primary-1': '#56DAFF',
+      'primary-2': '#1DBCC5',
+      'primary-3': '#E2E8F0',
+      'accent-1': '#E4434E',
+      'accent-2': '#FF5B6C',
+      'base-1': '#2E373F',
+      'base-2': '#FFF',
+      'black-0': '#000',
+      'black-300': '#333',
+      'black-600': '#666',
+      'black-900': '#999',
+      'white-0': '#fff',
+      'white-100': '#f2f2f2',
+    },
+    extend: {},
+  },
+  plugins: [],
+}
