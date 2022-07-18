a117d45366854684538ae95d6e1cba3a854dc0b6
YushiroDodo63 <yushiro0603@gmail.com>
2022-07-18 14:43:00 +0900

    [mod]NewButton to Button

--- view/next-project/src/components/common/index.ts
+++ view/next-project/src/components/common/index.ts
@@ -1,7 +1,8 @@
 export { default as Input } from './Input';
 export { default as Select } from './Select';
 export { default as Modal } from './Modal';
-export { default as NewButton } from './NewButton';
+export { default as Button } from './Button';
 export { default as PrimaryButton } from './PrimaryButton';
 export { default as AddButton } from './AddButton';
+export { default as RedButton } from './RedButton';
 export { default as Card } from './Card';
