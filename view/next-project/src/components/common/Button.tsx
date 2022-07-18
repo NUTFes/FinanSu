111d05520cf204ef7ab137688989fe84800de403
YushiroDodo63 <yushiro0603@gmail.com>
2022-07-18 14:17:08 +0900

    [mod]Buttonをchakraからtailwindに変更

--- view/next-project/src/components/common/Button.tsx
+++ view/next-project/src/components/common/Button.tsx
@@ -5,17 +5,15 @@ interface Props {
   onClick?: () => void;
 }
 
-const Button: React.FC<Props> = ({
-  children,
-  onClick,
-}) => {
+const Button: React.FC<Props> = ({ children, onClick }) => {
   return (
-    <button className={clsx("px-4 py-2 text-white-0 font-bold text-md rounded-lg bg-gradient-to-br from-primary-1 to-primary-2 hover:bg-primary-2")}
+    <button
+      className={clsx(
+        'px-4 py-2 text-white-0 font-bold text-md rounded-lg bg-gradient-to-br from-primary-1 to-primary-2 hover:bg-primary-2',
+      )}
       onClick={onClick}
     >
-      <div className={clsx("flex items-center")}>
-        {children}
-      </div>
+      <div className={clsx('flex items-center')}>{children}</div>
     </button>
   );
 };
