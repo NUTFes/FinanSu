8fa716c3e5d8c7a0b57129c8f1a3582cecab084f
YushiroDodo63 <yushiro0603@gmail.com>
2022-07-30 00:13:27 +0900

    [mod]購入報告の編集機能を実装(#349)

--- view/next-project/src/components/purchasereports/OpenEditModalButton.tsx
+++ view/next-project/src/components/purchasereports/OpenEditModalButton.tsx
@@ -1,38 +1,26 @@
-import { ChakraProvider, Button } from '@chakra-ui/react';
-import theme from '@assets/theme';
 import * as React from 'react';
 import EditModal from '@components/purchasereports/EditModal';
 import { useState } from 'react';
-import { RiPencilFill } from 'react-icons/ri';
+import { EditButton } from '@components/common';
 
 interface Props {
-  width?: string;
-  height?: string;
   children?: React.ReactNode;
   id: number;
 }
 
 const OpenEditModalButton: React.FC<Props> = (props) => {
-  const [showModal, setShowModal] = useState(false);
-  const ShowModal = () => {
-    setShowModal(true);
+  const [isOpen, setIsOpen] = useState(false);
+  const onOpen = () => {
+    setIsOpen(true);
   };
   return (
-    <ChakraProvider theme={theme}>
-      <Button
-        w='25px'
-        h='25px'
-        p='0'
-        minWidth='0'
-        borderRadius='full'
-        bgGradient='linear(to-br, primary.1 ,primary.2)'
-        onClick={ShowModal}
-      >
-        <RiPencilFill size={'15px'} color={'white'} />
-        {props.children}
-      </Button>
-      <EditModal id={props.id} openModal={showModal} setShowModal={setShowModal} />
-    </ChakraProvider>
+    <>
+      <EditButton onClick={onOpen} />
+      {
+        isOpen ? (
+          <EditModal purchaseReportId={props.id} isOpen={isOpen} setIsOpen={setIsOpen} />
+        ) : null}
+    </>
   );
 };
 
--- view/next-project/src/pages/purchasereports/index.tsx
+++ view/next-project/src/pages/purchasereports/index.tsx
@@ -2,9 +2,11 @@ import React, { useState } from 'react';
 import { get } from '@api/api_methods';
 import Head from 'next/head';
 import OpenAddModalButton from '@components/purchasereports/OpenAddModalButton';
+import OpenEditModalButton from '@components/purchasereports/OpenEditModalButton';
+import OpenDeleteModalButton from '@components/purchasereports/OpenDeleteModalButton';
 import MainLayout from '@components/layout/MainLayout';
 import clsx from 'clsx';
-import { Card, Title, Checkbox, EditButton, DeleteButton } from '@components/common';
+import { Card, Title, Checkbox } from '@components/common';
 import { useGlobalContext } from '@components/global/context';
 
 interface PurchaseReport {
@@ -151,9 +153,7 @@ export default function PurchaseReport(props: Props) {
       <Card>
         <div className={clsx('mt-10 mx-5')}>
           <div className={clsx('flex')}>
-            {/* <h1 className={clsx('text-2xl font-thin mr-5 mt-1 text-gray-900 align-text-bottom')}> */}
             <Title title={'購入報告一覧'} />
-            {/* </h1> */}
             <select className={clsx('w-100 ')}>
               <option value='2021'>2021</option>
               <option value='2022'>2022</option>
@@ -233,10 +233,10 @@ export default function PurchaseReport(props: Props) {
                   <td className={clsx('px-1', (index === 0 ? 'pt-4 pb-3' : 'py-3'), (index === purchaseReports.length - 1 ? 'pb-4 pt-3' : 'py-3 border-b'))}>
                     <div className={clsx('flex')}>
                       <div className={clsx('mx-1')}>
-                        <EditButton />
+                        <OpenEditModalButton id={purchaseReport.id} />
                       </div>
                       <div className={clsx('mx-1')}>
-                        <DeleteButton />
+                        <OpenDeleteModalButton id={purchaseReport.id} />
                       </div>
                     </div>
                   </td>
