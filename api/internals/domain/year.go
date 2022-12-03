793454b15fc76980f3602474c470eb94d2610b9b
ryuseifujisaki <fujiryu1122@gmail.com>
2022-12-03 23:05:23 +0900

    year.go ID→id

--- api/internals/domain/year.go
+++ api/internals/domain/year.go
@@ -5,7 +5,7 @@ import (
 )
 
 type Year struct {
-	ID        ID        `json:"ID"`
+	ID        ID        `json:"id"`
 	Year      int       `json:"year"`
 	CreatedAt time.Time `json:"createdAt"`
 	UpdatedAt time.Time `json:"updatedAt"`
