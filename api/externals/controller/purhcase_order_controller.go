7db3f622648d06b7db8075bfc2cda1f4c5487350
Kubosaka <s223301@stn.nagaokaut.ac.jp>
2023-02-08 10:45:57 +0900

    method-name-change

--- api/externals/controller/purhcase_order_controller.go
+++ api/externals/controller/purhcase_order_controller.go
@@ -1,9 +1,10 @@
 package controller
 
-import(
+import (
+	"net/http"
+
 	"github.com/NUTFes/FinanSu/api/internals/usecase"
 	"github.com/labstack/echo/v4"
-	"net/http"
 )
 
 type purchaseOrderController struct {
@@ -24,18 +25,18 @@ func NewPurchaseOrderController(u usecase.PurchaseOrderUseCase) PurchaseOrderCon
 	return &purchaseOrderController{u}
 }
 
-//Index
-func (p *purchaseOrderController) IndexPurchaseOrder(c echo.Context) error{
-	purchaseOrders , err :=p.u.GetPurchaseOrders(c.Request().Context())
+// Index
+func (p *purchaseOrderController) IndexPurchaseOrder(c echo.Context) error {
+	purchaseOrders, err := p.u.GetPurchaseOrders(c.Request().Context())
 	if err != nil {
 		return err
 	}
 	return c.JSON(http.StatusOK, purchaseOrders)
 }
 
-//show
-func (p *purchaseOrderController) ShowPurchaseOrder(c echo.Context) error{
-	id :=c.Param("id")
+// show
+func (p *purchaseOrderController) ShowPurchaseOrder(c echo.Context) error {
+	id := c.Param("id")
 	purchaseOrder, err := p.u.GetPurchaseOrderByID(c.Request().Context(), id)
 	if err != nil {
 		return err
@@ -43,34 +44,34 @@ func (p *purchaseOrderController) ShowPurchaseOrder(c echo.Context) error{
 	return c.JSON(http.StatusOK, purchaseOrder)
 }
 
-//Create
-func (p *purchaseOrderController) CreatePurchaseOrder(c echo.Context) error{
+// Create
+func (p *purchaseOrderController) CreatePurchaseOrder(c echo.Context) error {
 	deadLine := c.QueryParam("deadline")
 	userID := c.QueryParam("user_id")
 	financeCheck := c.QueryParam("finance_check")
-	purchaseOrder,err := p.u.CreatePurchaseOrder(c.Request().Context(), deadLine, userID, financeCheck)
+	purchaseOrder, err := p.u.CreatePurchaseOrder(c.Request().Context(), deadLine, userID, financeCheck)
 	if err != nil {
 		return err
 	}
 	return c.JSON(http.StatusOK, purchaseOrder)
 }
 
-//Update
-func (p *purchaseOrderController) UpdatePurchaseOrder(c echo.Context) error{
+// Update
+func (p *purchaseOrderController) UpdatePurchaseOrder(c echo.Context) error {
 	id := c.Param("id")
 	deadLine := c.QueryParam("deadline")
 	userID := c.QueryParam("user_id")
 	financeCheck := c.QueryParam("finance_check")
 
-	purchaseOrder,err := p.u.UpdatePurchaseOrder(c.Request().Context(), id, deadLine, userID, financeCheck)
+	purchaseOrder, err := p.u.UpdatePurchaseOrder(c.Request().Context(), id, deadLine, userID, financeCheck)
 	if err != nil {
 		return err
 	}
 	return c.JSON(http.StatusOK, purchaseOrder)
 }
 
-//Destory
-func (p *purchaseOrderController) DestroyPurchaseOrder(c echo.Context) error{
+// Destory
+func (p *purchaseOrderController) DestroyPurchaseOrder(c echo.Context) error {
 	id := c.Param("id")
 	err := p.u.DestroyPurchaseOrder(c.Request().Context(), id)
 	if err != nil {
@@ -79,22 +80,20 @@ func (p *purchaseOrderController) DestroyPurchaseOrder(c echo.Context) error{
 	return c.String(http.StatusOK, "Destroy PurchaseOrder")
 }
 
-//IndexOrderWithUserItem
-func (p *purchaseOrderController) IndexOrderDetail(c echo.Context) error{
-	orderWithUserItems, err := p.u.GetPurchaseOrderDetails(c.Request().Context())
+// IndexOrderWithUserItem
+func (p *purchaseOrderController) IndexOrderDetail(c echo.Context) error {
+	orderDetails, err := p.u.GetPurchaseOrderDetails(c.Request().Context())
 	if err != nil {
 		return err
 	}
-	return c.JSON(http.StatusOK,orderWithUserItems)
+	return c.JSON(http.StatusOK, orderDetails)
 }
 
-func (p *purchaseOrderController) ShowOrderDetail(c echo.Context) error{
+func (p *purchaseOrderController) ShowOrderDetail(c echo.Context) error {
 	id := c.Param("id")
-	orderWithUserAndItem , err := p.u.GetPurchaseOrderDetailByID(c.Request().Context(),id)
+	orderDetail, err := p.u.GetPurchaseOrderDetailByID(c.Request().Context(), id)
 	if err != nil {
 		return err
 	}
-	return c.JSON(http.StatusOK,orderWithUserAndItem)
+	return c.JSON(http.StatusOK, orderDetail)
 }
-
-
