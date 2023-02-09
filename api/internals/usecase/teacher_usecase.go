b4ebdaea976ff61df019f399add1bae681893765
ryuseifujisaki <fujiryu1122@gmail.com>
2023-02-09 16:34:49 +0900

    usecaseのリプレイス完了

--- api/internals/usecase/teacher_usecase.go
+++ api/internals/usecase/teacher_usecase.go
@@ -2,6 +2,7 @@ package usecase
 
 import (
 	"context"
+
 	rep "github.com/NUTFes/FinanSu/api/externals/repository"
 	"github.com/NUTFes/FinanSu/api/internals/domain"
 	"github.com/pkg/errors"
@@ -14,8 +15,8 @@ type teacherUseCase struct {
 type TeacherUseCase interface {
 	GetTeachers(context.Context) ([]domain.Teacher, error)
 	GetTeacherByID(context.Context, string) (domain.Teacher, error)
-	CreateTeacher(context.Context, string, string, string, string, string, string) error
-	UpdateTeacher(context.Context, string, string, string, string, string, string, string) error
+	CreateTeacher(context.Context, string, string, string, string, string, string) (domain.Teacher, error)
+	UpdateTeacher(context.Context, string, string, string, string, string, string, string) (domain.Teacher, error)
 	DestroyTeacher(context.Context, string) error
 }
 
@@ -80,14 +81,67 @@ func (t *teacherUseCase) GetTeacherByID(c context.Context, id string) (domain.Te
 	return teacher, nil
 }
 
-func (t *teacherUseCase) CreateTeacher(c context.Context, name string, position string, departmentID string, room string, isBlack string, remark string) error {
+func (t *teacherUseCase) CreateTeacher(
+	c context.Context,
+	name string,
+	position string,
+	departmentID string,
+	room string,
+	isBlack string,
+	remark string) (domain.Teacher, error) {
+
 	err := t.rep.Create(c, name, position, departmentID, room, isBlack, remark)
-	return err
+	latestTeacher := domain.Teacher{}
+
+	row, err := t.rep.FindLatestRecord(c)
+	err = row.Scan(
+		&latestTeacher.ID,
+		&latestTeacher.Name,
+		&latestTeacher.Position,
+		&latestTeacher.DepartmentID,
+		&latestTeacher.Room,
+		&latestTeacher.IsBlack,
+		&latestTeacher.Remark,
+		&latestTeacher.CreatedAt,
+		&latestTeacher.UpdatedAt,
+	)
+
+	if err != nil {
+		return latestTeacher, err
+	}
+	return latestTeacher, err
 }
 
-func (t *teacherUseCase) UpdateTeacher(c context.Context, id string, name string, position string, departmentID string, room string, isBlack string, remark string) error {
+func (t *teacherUseCase) UpdateTeacher(
+	c context.Context,
+	id string,
+	name string,
+	position string,
+	departmentID string,
+	room string,
+	isBlack string,
+	remark string) (domain.Teacher, error) {
+
 	err := t.rep.Update(c, id, name, position, departmentID, room, isBlack, remark)
-	return err
+	updateTeacher := domain.Teacher{}
+
+	row, err := t.rep.Find(c, id)
+	err = row.Scan(
+		&updateTeacher.ID,
+		&updateTeacher.Name,
+		&updateTeacher.Position,
+		&updateTeacher.DepartmentID,
+		&updateTeacher.Room,
+		&updateTeacher.IsBlack,
+		&updateTeacher.Remark,
+		&updateTeacher.CreatedAt,
+		&updateTeacher.UpdatedAt,
+	)
+
+	if err != nil {
+		return updateTeacher, err
+	}
+	return updateTeacher, err
 }
 
 func (t *teacherUseCase) DestroyTeacher(c context.Context, id string) error {
