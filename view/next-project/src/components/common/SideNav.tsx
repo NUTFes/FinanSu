ada22b9a93d53869d9c7f3c54e2762d501766003
imaimai17468 <imaimai17468@users.noreply.github.com>
2023-02-14 05:07:56 +0000

    formatted by workflow

--- view/next-project/src/components/common/SideNav.tsx
+++ view/next-project/src/components/common/SideNav.tsx
@@ -1,10 +1,10 @@
 import clsx from 'clsx';
 import { useRouter } from 'next/router';
 import React, { ReactNode, useReducer } from 'react';
-import { HiOutlineShoppingCart, HiOutlineDocumentText, HiCurrencyDollar } from 'react-icons/hi';
-import { MdOutlineSavings, MdOutlineWorkOutline } from 'react-icons/md';
 import { BsBuilding, BsVectorPen } from 'react-icons/bs';
+import { HiOutlineShoppingCart, HiOutlineDocumentText, HiCurrencyDollar } from 'react-icons/hi';
 import { IoIosArrowDropup } from 'react-icons/io';
+import { MdOutlineSavings, MdOutlineWorkOutline } from 'react-icons/md';
 
 interface LinkItemProps {
   name: string;
@@ -89,14 +89,30 @@ export default function SimpleSidebar() {
     <div className={clsx('h-full w-full bg-primary-4')}>
       <div className={clsx('border-b-2 border-primary-1')}>
         {FinanceLinkItems.map((link) => (
-          <NavItem key={link.name} icon={link.icon} href={link.href} currentPath={router.pathname} isParent={link.isParent} isShow={financeItemsShow} onClick={link.isParent?setFinanceItemsShow:undefined}>
+          <NavItem
+            key={link.name}
+            icon={link.icon}
+            href={link.href}
+            currentPath={router.pathname}
+            isParent={link.isParent}
+            isShow={financeItemsShow}
+            onClick={link.isParent ? setFinanceItemsShow : undefined}
+          >
             {link.name}
           </NavItem>
         ))}
       </div>
       <div className={clsx('border-b-2 border-primary-1')}>
         {RelationLinkItems.map((link) => (
-          <NavItem key={link.name} icon={link.icon} href={link.href} currentPath={router.pathname} isParent={link.isParent} isShow={relationItemsShow} onClick={link.isParent?setRelationItemsShow:undefined}>
+          <NavItem
+            key={link.name}
+            icon={link.icon}
+            href={link.href}
+            currentPath={router.pathname}
+            isParent={link.isParent}
+            isShow={relationItemsShow}
+            onClick={link.isParent ? setRelationItemsShow : undefined}
+          >
             {link.name}
           </NavItem>
         ))}
@@ -107,7 +123,7 @@ export default function SimpleSidebar() {
 
 const NavItem = (props: NavItemProps) => {
   let className = '';
-  
+
   if (props.currentPath === props.href) {
     className = 'flex items-center w-full text-primary-5 bg-white-0 px-2 py-4';
   } else {
@@ -115,17 +131,29 @@ const NavItem = (props: NavItemProps) => {
       'flex items-center w-full text-white-0 bg-primary-5 px-2 py-4 hover:bg-primary-4 transition-all';
   }
 
-  if(props.isShow && !props.isParent){
+  if (props.isShow && !props.isParent) {
     className += ' hidden';
   }
 
   return (
     <>
       {props.isParent ? (
-          <div className={clsx(className + ` cursor-pointer ${!props.isShow && 'border-b-2 border-dashed border-primary-1'}`)} onClick={ props.onClick }>
-            <span className={clsx(props.isShow ? 'rotate-180 transition-transform' : 'rotate-0 transition-transform')}>{props.icon}</span>
-            {props.children}
-          </div>
+        <div
+          className={clsx(
+            className +
+              ` cursor-pointer ${!props.isShow && 'border-b-2 border-dashed border-primary-1'}`,
+          )}
+          onClick={props.onClick}
+        >
+          <span
+            className={clsx(
+              props.isShow ? 'rotate-180 transition-transform' : 'rotate-0 transition-transform',
+            )}
+          >
+            {props.icon}
+          </span>
+          {props.children}
+        </div>
       ) : (
         <a href={props.href}>
           <div className={clsx(className)}>
