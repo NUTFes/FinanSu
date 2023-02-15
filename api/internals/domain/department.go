ab8213eb04b0d42858ad7a875a56f19805a4911d
Kubosaka <s223301@stn.nagaokaut.ac.jp>
2023-02-15 11:46:50 +0900

    425-fix-department

--- api/internals/domain/department.go
+++ api/internals/domain/department.go
@@ -5,7 +5,7 @@ import (
 )
 
 type Department struct {
-	ID        ID        `json:"id"`
+	ID        int       `json:"id"`
 	Name      string    `json:"name"`
 	CreatedAt time.Time `json:"createdAt"`
 	UpdatedAt time.Time `json:"updatedAt"`
