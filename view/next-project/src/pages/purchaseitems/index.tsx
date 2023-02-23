f959cb64584f53306a834137a27f9af9ec59b187
imaimai17468 <tosiki_801_life@outlook.jp>
2023-02-23 17:42:59 +0900

    [fix] Head要素を追加してChromeのタブで見れるようにする

--- view/next-project/src/pages/purchaseitems/index.tsx
+++ view/next-project/src/pages/purchaseitems/index.tsx
@@ -1,3 +1,4 @@
+import Head from 'next/head';
 import {
   Box,
   Button,
@@ -58,6 +59,10 @@ export default function PurchaseItems(props: Props) {
   return (
     <>
       <MainLayout>
+      <Head>
+        <title>協賛物品一覧</title>
+        <meta name='viewport' content='initial-scale=1.0, width=device-width' />
+      </Head>
         <Flex justify='center' align='center'>
           <Center>
             <Box m='10' px='10' boxShadow='base' rounded='lg'>
