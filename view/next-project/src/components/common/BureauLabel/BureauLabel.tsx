e5ba6a3c65dbc3ac49ddfdd9fa324bc6f9f5fe3b
imaimai17468 <tosiki_801_life@outlook.jp>
2023-03-01 15:40:34 +0900

    [feat] 各局のラベルを追加

---
+++ view/next-project/src/components/common/BureauLabel/BureauLabel.tsx
@@ -0,0 +1,19 @@
+import { BureauLabelProps } from './BureauLabel.type';
+import React, { useMemo } from 'react';
+import Label from '../Label';
+import { BUREAUS_WITH_EN } from '@constants/bureaus';
+import { Color } from '@type/color.type';
+
+const BureauLabel: React.FC<BureauLabelProps> = (props) => {
+  const { bureauID } = props;
+  const bureau = useMemo(() => {
+    const bureau = BUREAUS_WITH_EN.find(
+      (bureau): bureau is { id: number; name: string; name_en: Color } => bureau.id === bureauID,
+    ); 
+    return bureau;
+  }, [bureauID]);
+
+  return <Label color={bureau?.name_en}>{bureau?.name}</Label>;
+};
+
+export default BureauLabel;
---
+++ view/next-project/src/components/common/BureauLabel/BureauLabel.type.ts
@@ -0,0 +1,4 @@
+
+export type BureauLabelProps = {
+  bureauID: Number;
+}
