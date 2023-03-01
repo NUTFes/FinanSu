1a5d4b8fe5ad233529d61e2f2e6d3bafa0a89de0
imaimai17468 <tosiki_801_life@outlook.jp>
2023-03-01 11:36:55 +0900

    [fix]局一覧をconstantsに移動

---
+++ view/next-project/src/constants/bureaus.ts
@@ -0,0 +1,28 @@
+import { Bureau } from '../type/common';
+
+export const BUREAUS = [
+  {
+    id: 1,
+    name: '総務局',
+  },
+  {
+    id: 2,
+    name: '渉外局',
+  },
+  {
+    id: 3,
+    name: '財務局',
+  },
+  {
+    id: 4,
+    name: '企画局',
+  },
+  {
+    id: 5,
+    name: '制作局',
+  },
+  {
+    id: 6,
+    name: '情報局',
+  },
+] as Bureau[];
