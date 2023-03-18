af3825e43a9dbbceb8b2154b1bc85ca7c381c341
imaimai17468 <tosiki_801_life@outlook.jp>
2023-03-18 15:48:58 +0900

    [fix] editボタンの分岐を省コード化

--- view/next-project/src/components/common/EditButton.tsx
+++ view/next-project/src/components/common/EditButton.tsx
@@ -1,5 +1,4 @@
-import clsx from 'clsx';
-import React from 'react';
+import React, { useMemo } from 'react';
 import { RiPencilFill } from 'react-icons/ri';
 
 interface Props {
@@ -7,34 +6,23 @@ interface Props {
   isDisabled?: boolean;
 }
 
-function EditButton(props: Props): JSX.Element {
+const EditButton: React.FC<Props> = (props) => {
+  const { onClick, isDisabled = true } = props;
+
   return (
-    <>
-      {props.isDisabled ? (
-        <button
-          className={clsx(
-            'h-6 w-6 min-w-0 rounded-full bg-gradient-to-br from-primary-1 to-primary-2 p-0 hover:bg-gradient-to-br hover:from-primary-2 hover:to-primary-1',
-          )}
-          onClick={props.onClick}
-        >
-          <div className={clsx('flex grid items-center justify-items-center')}>
-            <RiPencilFill size={'15px'} color={'white'} />
-          </div>
-        </button>
-      ) : (
-        <button
-          disabled
-          className={clsx(
-            'h-6 w-6 min-w-0 rounded-full bg-gradient-to-br from-primary-1 to-primary-2 p-0 opacity-25',
-          )}
-          onClick={props.onClick}
-        >
-          <div className={clsx('flex grid items-center justify-items-center')}>
-            <RiPencilFill size={'15px'} color={'white'} />
-          </div>
-        </button>
-      )}
-    </>
+    <button
+      className={`${
+      !isDisabled
+          ? 'cursor-default bg-primary-1 opacity-25'
+          : 'cursor-pointer bg-gradient-to-br from-primary-1 to-primary-2 hover:bg-gradient-to-br hover:from-primary-2 hover:to-primary-1'
+      }  h-6 w-6 min-w-0 rounded-full  p-0`}
+      disabled={!isDisabled}
+      onClick={onClick}
+    >
+      <div className='flex items-center justify-center'>
+        <RiPencilFill size={'15px'} color={'white'} />
+      </div>
+    </button>
   );
 }
 
