5220f1051f1992d0e955584dd716ec4c25406996
imaimai17468 <tosiki_801_life@outlook.jp>
2023-03-26 21:08:35 +0900

    [fix] primary buttonにtypeとdisabledを追加

--- view/next-project/src/components/common/PrimaryButton/PrimaryButton.tsx
+++ view/next-project/src/components/common/PrimaryButton/PrimaryButton.tsx
@@ -5,14 +5,17 @@ interface Props {
   className?: string;
   onClick?: () => void;
   children?: React.ReactNode;
+  type?: 'button' | 'submit' | 'reset';
+  disabled?: boolean;
 }
 
 function PrimaryButton(props: Props): JSX.Element {
   const className =
     'px-4 py-2 text-white-0 font-bold text-md rounded-lg bg-gradient-to-br from-primary-1 to-primary-2 hover:bg-gradient-to-br hover:from-primary-2 hover:to-primary-1' +
-    (props.className ? ` ${props.className}` : '');
+    (props.className ? ` ${props.className}` : '') +
+    (props.disabled ? ' cursor-not-allowed opacity-20' : '');
   return (
-    <button className={clsx(className)} onClick={props.onClick}>
+    <button className={clsx(className)} onClick={props.onClick} type={props.type} disabled={props.disabled}>
       <div className={clsx('flex items-center')}>{props.children}</div>
     </button>
   );
