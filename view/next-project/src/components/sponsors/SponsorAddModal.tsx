0aae63f3ae9d3c42cc51731a56e7290868fd7ee6
imaimai17468 <tosiki_801_life@outlook.jp>
2023-03-30 16:59:00 +0900

    [fix] 不要なブランケットを削除

--- view/next-project/src/components/sponsors/SponsorAddModal.tsx
+++ view/next-project/src/components/sponsors/SponsorAddModal.tsx
@@ -36,49 +36,47 @@ export default function SponsorAddModal() {
   };
 
   return (
-    <>
-      <Modal className='w-1/2'>
-        <div className='w-full'>
-          <div className='mr-5 w-fit ml-auto'>
-            <CloseButton onClick={closeModal} />
-          </div>
+    <Modal className='w-1/2'>
+      <div className='w-full'>
+        <div className='mr-5 w-fit ml-auto'>
+          <CloseButton onClick={closeModal} />
         </div>
-        <h1 className='mb-10 w-fit mx-auto text-xl text-black-600'>
-          企業登録
-        </h1>
-        <div className='my-6 grid grid-cols-5 gap-4 items-center justify-items-center'>
-          <p className='col-span-1 text-black-600'>企業名</p>
-          <div className='col-span-4 w-full'>
-            <Input className='w-full' value={formData.name} onChange={handler('name')} />
-          </div>
-          <p className='col-span-1 text-black-600'>電話番号</p>
-          <div className='col-span-4 w-full'>
-            <Input className='w-full' value={formData.tel} onChange={handler('tel')} />
-          </div>
-          <p className='col-span-1 text-black-600'>メール</p>
-          <div className='col-span-4 w-full'>
-            <Input className='w-full' value={formData.email} onChange={handler('email')} />
-          </div>
-          <p className='col-span-1 text-black-600'>住所</p>
-          <div className='col-span-4 w-full'>
-            <Input className='w-full' value={formData.address} onChange={handler('address')} />
-          </div>
-          <p className='col-span-1 text-black-600'>代表者</p>
-          <div className='col-span-4 w-full'>
-            <Input className='w-full' value={formData.representative} onChange={handler('representative')} />
-          </div>
+      </div>
+      <h1 className='mb-10 w-fit mx-auto text-xl text-black-600'>
+        企業登録
+      </h1>
+      <div className='my-6 grid grid-cols-5 gap-4 items-center justify-items-center'>
+        <p className='col-span-1 text-black-600'>企業名</p>
+        <div className='col-span-4 w-full'>
+          <Input className='w-full' value={formData.name} onChange={handler('name')} />
         </div>
-        <div className='mb-5 w-fit mx-auto'>
-          <PrimaryButton
-            className={'mx-2'}
-            onClick={() => {
-              submit(formData);
-            }}
-          >
-            登録する
-          </PrimaryButton>
+        <p className='col-span-1 text-black-600'>電話番号</p>
+        <div className='col-span-4 w-full'>
+          <Input className='w-full' value={formData.tel} onChange={handler('tel')} />
         </div>
-      </Modal>
-    </>
+        <p className='col-span-1 text-black-600'>メール</p>
+        <div className='col-span-4 w-full'>
+          <Input className='w-full' value={formData.email} onChange={handler('email')} />
+        </div>
+        <p className='col-span-1 text-black-600'>住所</p>
+        <div className='col-span-4 w-full'>
+          <Input className='w-full' value={formData.address} onChange={handler('address')} />
+        </div>
+        <p className='col-span-1 text-black-600'>代表者</p>
+        <div className='col-span-4 w-full'>
+          <Input className='w-full' value={formData.representative} onChange={handler('representative')} />
+        </div>
+      </div>
+      <div className='mb-5 w-fit mx-auto'>
+        <PrimaryButton
+          className={'mx-2'}
+          onClick={() => {
+            submit(formData);
+          }}
+        >
+          登録する
+        </PrimaryButton>
+      </div>
+    </Modal>
   );
 }
