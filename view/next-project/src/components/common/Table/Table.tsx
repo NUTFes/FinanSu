a025f14afd2797f4c23a3bffa45e7a20a4e4b6cd
imaimai17468 <tosiki_801_life@outlook.jp>
2023-04-14 10:48:15 +0900

    [feat] tableコンポーネントの追加

---
+++ view/next-project/src/components/common/Table/Table.tsx
@@ -0,0 +1,80 @@
+import React, { FC, useState } from 'react';
+import s from './Table.module.css';
+import DataTable from 'react-data-table-component';
+import { useRouter } from 'next/router';
+import { ReactNode } from 'react';
+import Input from '../Input/Input';
+import CloseButton from '../CloseButton';
+
+interface Column {
+  name: ReactNode;
+  selector: (row: any) => string;
+  sortable: boolean;
+  style?: any;
+}
+
+interface TableContentProps {
+  columns: Column[];
+  data: any;
+}
+
+const CUSTOM_STYLES = {
+  //   headCells: {
+  //     style: {
+  //       fontSize: '14px',
+  //       justifyContent: 'center',
+  //     },
+  //   },
+  //   cells: {
+  //     style: {
+  //       justifyContent: 'center',
+  //       padding: '25px',
+  //     },
+  //   },
+  //   headRow: {
+  //     style: {
+  //       borderBottomWidth: '1px',
+  //       borderBottomColor: '#000',
+  //     },
+  //   },
+  //   rows: {
+  //     highlightOnHoverStyle: {
+  //       transform: 'translateY(-1px)',
+  //       backgroundColor: 'white',
+  //       boxShadow: '5px 5px 14px #f0f0f0, -5px -5px 14px #fafafa',
+  //       transition: 'all 0.2s ease-in-out',
+  //     },
+  //   },
+};
+
+const Table: FC<TableContentProps> = (props) => {
+  const router = useRouter();
+  const [filterText, setFilterText] = useState('');
+
+  const filteredItems = props.data.filter(
+    (item: any) => JSON.stringify(item).toLowerCase().indexOf(filterText.toLowerCase()) !== -1,
+  );
+
+  return (
+    <>
+      <div className='flex gap-3'>
+        <Input
+          placeholder='絞り込み'
+          value={filterText}
+          onChange={(e) => setFilterText(e.target.value)}
+        />
+        <CloseButton onClick={() => setFilterText('')} />
+      </div>
+      <DataTable
+        columns={props.columns}
+        data={filteredItems}
+        pagination
+        highlightOnHover
+        pointerOnHover
+        customStyles={CUSTOM_STYLES}
+      />
+    </>
+  );
+};
+
+export default Table;
