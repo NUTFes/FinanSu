1fa40ecce2efc10c150b3799213d5fa8149f6731
Kubosaka <s223301@stn.nagaokaut.ac.jp>
2023-04-26 14:06:25 +0900

    seedデータ変更

--- mysql/db/mail_auth.sql
+++ mysql/db/mail_auth.sql
@@ -10,7 +10,7 @@ CREATE TABLE mail_auth (
   PRIMARY KEY (id)
 );
 
-INSERT INTO mail_auth (email, password, user_id) VALUES ( "test1@test.com" , "$2a$10$vtmStGYxc8/HdhBYJWyztOMrdpw1QaKxsuGdi72i8iqeyp.1997ke" , 1);
-INSERT INTO mail_auth (email, password, user_id) VALUES ( "test2@test.com" , "$2a$10$vtmStGYxc8/HdhBYJWyztOMrdpw1QaKxsuGdi72i8iqeyp.1997ke" , 2);
-INSERT INTO mail_auth (email, password, user_id) VALUES ( "test3@test.com" , "$2a$10$vtmStGYxc8/HdhBYJWyztOMrdpw1QaKxsuGdi72i8iqeyp.1997ke" , 3);
-INSERT INTO mail_auth (email, password, user_id) VALUES ( "test4@test.com" , "$2a$10$vtmStGYxc8/HdhBYJWyztOMrdpw1QaKxsuGdi72i8iqeyp.1997ke" , 4);
+INSERT INTO mail_auth (email, password, user_id) VALUES ( "test1@example.com" , "$2a$10$vtmStGYxc8/HdhBYJWyztOMrdpw1QaKxsuGdi72i8iqeyp.1997ke" , 1);
+INSERT INTO mail_auth (email, password, user_id) VALUES ( "test2@example.com" , "$2a$10$vtmStGYxc8/HdhBYJWyztOMrdpw1QaKxsuGdi72i8iqeyp.1997ke" , 2);
+INSERT INTO mail_auth (email, password, user_id) VALUES ( "test3@example.com" , "$2a$10$vtmStGYxc8/HdhBYJWyztOMrdpw1QaKxsuGdi72i8iqeyp.1997ke" , 3);
+INSERT INTO mail_auth (email, password, user_id) VALUES ( "test4@example.com" , "$2a$10$vtmStGYxc8/HdhBYJWyztOMrdpw1QaKxsuGdi72i8iqeyp.1997ke" , 4);
