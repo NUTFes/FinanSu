cf16e47c650225b2101171a384e937fba01c2065
Kubosaka <s223301@stn.nagaokaut.ac.jp>
2023-05-10 14:09:17 +0900

    カード2追加

--- view/next-project/src/components/common/Card/Card.tsx
+++ view/next-project/src/components/common/Card/Card.tsx
@@ -8,10 +8,10 @@ interface Props {
 
 function Card(props: Props): JSX.Element {
   return (
-    <div className={clsx('mx-auto', props.w ? props.w : 'w-4/5')}>
+    <div className={clsx('md:mx-auto', props.w ? props.w : 'md:w-4/5')}>
       <div
         className={clsx(
-          'm-10 rounded-lg border border-black-300 border-opacity-10 px-10 shadow-md',
+          '-mx-10 mr-2 m-2 md:m-10 rounded-lg border border-black-300 border-opacity-10 px-10 shadow-md',
         )}
       >
         {props.children}
---
+++ view/next-project/src/components/common/Card2/Card2.tsx
@@ -0,0 +1,21 @@
+import clsx from 'clsx';
+import React from 'react';
+
+interface Props {
+  w?: string;
+  children?: React.ReactNode;
+}
+
+function Card(props: Props): JSX.Element {
+  return (
+    <div
+      className={clsx(
+        'm-auto mt-3 mb-2 w-full  border border-black-300 border-opacity-10 px-5 shadow-md rounded-lg md:hidden',
+      )}
+    >
+      {props.children}
+    </div>
+  );
+}
+
+export default Card;
