154bcde749905c24d89769895f18a022030aa594
KazumaSun <satoma.2001.0920@gmail.com>
2023-05-11 17:12:13 +0000

    [fix]seedsの変更

--- mysql/db/purcahse_items.sql
+++ mysql/db/purcahse_items.sql
@@ -5,6 +5,7 @@ CREATE TABLE purchase_items (
   item varchar(255),
   price int(10),
   quantity int(10),
+  source_id int(10),
   detail varchar(255),
   url varchar(255),
   purchase_order_id int(10),
@@ -14,12 +15,12 @@ CREATE TABLE purchase_items (
   PRIMARY KEY (id)
 );
 
-INSERT into purchase_items (item, price, quantity, detail, url, purchase_order_id, finance_check )values ('ビンゴ景品', 10000, 1, 'test-detail', 'https://nutfes.net', 1, true);
-INSERT into purchase_items (item, price, quantity, detail, url, purchase_order_id, finance_check )values ('ヒーロースーツ', 20000, 2, 'test-detail2', 'https://nutfes.net', 1, false);
-INSERT into purchase_items (item, price, quantity, detail, url, purchase_order_id, finance_check )values ('ビンゴ用品', 30000, 3, 'test-detail3', 'https://nutfes.net', 2, true); 
-INSERT into purchase_items (item, price, quantity, detail, url, purchase_order_id, finance_check )values ('マイク', 10000, 1, 'test-detail', 'https://nutfes.net', 2, true);
-INSERT into purchase_items (item, price, quantity, detail, url, purchase_order_id, finance_check )values ('机', 20000, 2, 'test-detail2', 'https://nutfes.net', 3, true);
-INSERT into purchase_items (item, price, quantity, detail, url, purchase_order_id, finance_check )values ('椅子', 30000, 3, 'test-detail3', 'https://nutfes.net', 3,  false);
-INSERT into purchase_items (item, price, quantity, detail, url, purchase_order_id, finance_check )values ('酒', 10000, 10, 'test-detail', 'https://nutfes.net', 4, true);
-INSERT into purchase_items (item, price, quantity, detail, url, purchase_order_id, finance_check )values ('封筒', 100, 100, 'test-detail2', 'https://nutfes.net', 5, false);
-INSERT into purchase_items (item, price, quantity, detail, url, purchase_order_id, finance_check )values ('領収書', 100, 50, 'test-detail3', 'https://nutfes.net', 6, true);
+INSERT into purchase_items (item, price, quantity, source_id, detail, url, purchase_order_id, finance_check )values ('ビンゴ景品', 10000, 1, 1, 'test-detail', 'https://nutfes.net', 1, true);
+INSERT into purchase_items (item, price, quantity, source_id, detail, url, purchase_order_id, finance_check )values ('ヒーロースーツ', 20000, 2, 1, 'test-detail2', 'https://nutfes.net', 1, false);
+INSERT into purchase_items (item, price, quantity, source_id, detail, url, purchase_order_id, finance_check )values ('ビンゴ用品', 30000, 3, 2, 'test-detail3', 'https://nutfes.net', 2, true); 
+INSERT into purchase_items (item, price, quantity, source_id, detail, url, purchase_order_id, finance_check )values ('マイク', 10000, 1, 1, 'test-detail', 'https://nutfes.net', 2, true);
+INSERT into purchase_items (item, price, quantity, source_id, detail, url, purchase_order_id, finance_check )values ('机', 20000, 2, 1, 'test-detail2', 'https://nutfes.net', 3, true);
+INSERT into purchase_items (item, price, quantity, source_id, detail, url, purchase_order_id, finance_check )values ('椅子', 30000, 3, 2, 'test-detail3', 'https://nutfes.net', 3,  false);
+INSERT into purchase_items (item, price, quantity, source_id, detail, url, purchase_order_id, finance_check )values ('酒', 10000, 10, 1, 'test-detail', 'https://nutfes.net', 4, true);
+INSERT into purchase_items (item, price, quantity, source_id, detail, url, purchase_order_id, finance_check )values ('封筒', 100, 100, 1, 'test-detail2', 'https://nutfes.net', 5, false);
+INSERT into purchase_items (item, price, quantity, source_id, detail, url, purchase_order_id, finance_check )values ('領収書', 100, 50, 2, 'test-detail3', 'https://nutfes.net', 6, true);
--- mysql/db/purchase_orders.sql
+++ mysql/db/purchase_orders.sql
@@ -5,16 +5,15 @@ CREATE TABLE purchase_orders (
   deadline varchar(255),
   user_id int(10),
   expense_id int(10),
-  source_id int(10),
   finance_check boolean,
   created_at datetime not null default current_timestamp,
   updated_at datetime not null default current_timestamp on update current_timestamp,
   PRIMARY KEY (id)
 );
 
-INSERT into purchase_orders (deadline, user_id, expense_id, source_id, finance_check) values ('2022-2-22', 1, 1, 1, true);
-INSERT into purchase_orders (deadline, user_id, expense_id, source_id, finance_check) values ('2022-3-28', 1, 1, 1, true);
-INSERT into purchase_orders (deadline, user_id, expense_id, source_id, finance_check) values ('2022-4-6', 1, 2, 2, true);
-INSERT into purchase_orders (deadline, user_id, expense_id, source_id, finance_check) values ('2022-3-28', 1, 3, 1, false);
-INSERT into purchase_orders (deadline, user_id, expense_id, source_id, finance_check) values ('2022-4-6', 1, 4, 2, true);
-INSERT into purchase_orders (deadline, user_id, expense_id, source_id, finance_check) values ('2022-4-6', 1, 5, 2, true);
+INSERT into purchase_orders (deadline, user_id, expense_id, finance_check) values ('2022-2-22', 1, 1, true);
+INSERT into purchase_orders (deadline, user_id, expense_id, finance_check) values ('2022-3-28', 1, 1, true);
+INSERT into purchase_orders (deadline, user_id, expense_id, finance_check) values ('2022-4-6', 1, 2, true);
+INSERT into purchase_orders (deadline, user_id, expense_id, finance_check) values ('2022-3-28', 1, 3, false);
+INSERT into purchase_orders (deadline, user_id, expense_id, finance_check) values ('2022-4-6', 1, 4, true);
+INSERT into purchase_orders (deadline, user_id, expense_id, finance_check) values ('2022-4-6', 1, 5, true);
