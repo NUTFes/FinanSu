4cddbdd5ba47f3f241ce0a76717e38b78aefa5d6
Kubosaka <s223301@stn.nagaokaut.ac.jp>
2023-05-12 10:56:46 +0900

    中間テーブルの作成

--- mysql/db/activities.sql
+++ mysql/db/activities.sql
@@ -16,3 +16,15 @@ CREATE TABLE activities (
 
 INSERT into activities (sponsor_style_id, user_id, is_done, sponsor_id, feature, expense, remark) values (1, 1, false, 1, "なし", 11, "");
 INSERT into activities (sponsor_style_id, user_id, is_done, sponsor_id, feature, expense, remark) values (2, 2, false, 2, "クーポン", 22, "味玉or大盛無料");
+
+CREATE TABLE activitiy_styles (
+  id int(10) unsigned not null auto_increment,
+  activity_id int(10),
+  sponsor_style_id int(10),
+  created_at datetime not null default current_timestamp,
+  updated_at datetime not null default current_timestamp on update current_timestamp,
+  PRIMARY KEY (id)
+);
+
+INSERT into activitiy_styles (activity_id, sponsor_style_id) values (1, 1);
+INSERT into activitiy_styles (activity_id, sponsor_style_id) values (2, 2);
