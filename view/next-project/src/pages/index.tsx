d7b08bf0a6eb9e5c1bab332aa2b9c7fd27b08555
imaimai17468 <imaimai17468@users.noreply.github.com>
2023-05-13 09:58:16 +0000

    formatted by workflow

--- view/next-project/src/pages/index.tsx
+++ view/next-project/src/pages/index.tsx
@@ -40,9 +40,9 @@ export default function Home() {
               alt='logo'
               width={150}
               height={40}
-              className='h-fit md:w-48 w-40'
+              className='h-fit w-40 md:w-48'
             />
-            <p className='text-2xl md:text-3xl text-black-600'>新規登録</p>
+            <p className='text-2xl text-black-600 md:text-3xl'>新規登録</p>
           </div>
           <SignUpView />
           <hr className='border-black-300' />
@@ -56,7 +56,7 @@ export default function Home() {
   };
   return (
     <LoginLayout>
-      <div className='md:w-1/2 m-4 w-fit rounded-lg px-5 shadow-md md:m-8 md:px-10'>
+      <div className='m-4 w-fit rounded-lg px-5 shadow-md md:m-8 md:w-1/2 md:px-10'>
         {cardContent(isMember)}
       </div>
     </LoginLayout>
