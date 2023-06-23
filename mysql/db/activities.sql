5d2757c8f92cfe4bb7f78df5b93ada8e7ac30526
Kubosaka <s223301@stn.nagaokaut.ac.jp>
2023-06-23 18:33:19 +0900

    api修正

--- mysql/db/activities.sql
+++ mysql/db/activities.sql
@@ -8,13 +8,15 @@ CREATE TABLE activities (
   feature varchar(255),
   expense int(10),
   remark varchar(255),
+  design int(10),
+  url varchar(255),
   created_at datetime not null default current_timestamp,
   updated_at datetime not null default current_timestamp on update current_timestamp,
   PRIMARY KEY (id)
 );
 
-INSERT into activities (user_id, is_done, sponsor_id, feature, expense, remark) values (1, false, 1, "なし", 11, "");
-INSERT into activities (user_id, is_done, sponsor_id, feature, expense, remark) values (2, false, 2, "クーポン", 22, "味玉or大盛無料");
+INSERT into activities (user_id, is_done, sponsor_id, feature, expense, remark, design, url) values (1, false, 1, "なし", 11, "" ,1 ,"");
+INSERT into activities (user_id, is_done, sponsor_id, feature, expense, remark, design, url) values (2, false, 2, "クーポン", 22, "味玉or大盛無料",1 ,"");
 
 CREATE TABLE activity_styles (
   id int(10) unsigned not null auto_increment,
