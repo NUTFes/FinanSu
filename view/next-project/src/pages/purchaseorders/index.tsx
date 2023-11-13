5ab737e67418211c18768b00fea7166638ef41ed
Kubosaka <s223301@stn.nagaokaut.ac.jp>
2023-11-13 21:46:31 +0900

    購入申請ページ年度更新

--- view/next-project/src/pages/purchaseorders/index.tsx
+++ view/next-project/src/pages/purchaseorders/index.tsx
@@ -22,11 +22,12 @@ interface Props {
   expenses: Expense[];
 }
 export async function getServerSideProps() {
-  const getPurchaseOrderViewUrl = process.env.SSR_API_URI + '/purchaseorders/details';
+  const currentYear = '2024';
+  const getPurchaseOrderViewUrl =
+    process.env.SSR_API_URI + '/purchaseorders/details/' + currentYear;
   const getExpenseUrl = process.env.SSR_API_URI + '/expenses';
   const purchaseOrderViewRes = await get(getPurchaseOrderViewUrl);
   const expenseRes = await get(getExpenseUrl);
-
   return {
     props: {
       purchaseOrderView: purchaseOrderViewRes,
@@ -64,14 +65,15 @@ export default function PurchaseOrders(props: Props) {
     return datetime2;
   };
 
-  const currentYear = new Date().getFullYear().toString();
-  const [selectedYear, setSelectedYear] = useState<string>(currentYear);
+  const [selectedYear, setSelectedYear] = useState<string>('2024');
 
-  const filteredPurchaseOrderViews = useMemo(() => {
-    return purchaseOrderViews.filter((purchaseOrderView: PurchaseOrderView) => {
-      return purchaseOrderView.purchaseOrder.createdAt?.includes(selectedYear);
-    });
-  }, [purchaseOrderViews, selectedYear]);
+  const getPurchaseOrders = async () => {
+    const getPurchaseOrderViewUrlByYear =
+      process.env.CSR_API_URI + '/purchaseorders/details/' + selectedYear;
+    console.log(getPurchaseOrderViewUrlByYear);
+    const getPurchaseOrderByYears = await get(getPurchaseOrderViewUrlByYear);
+    setPurchaseOrderViews(getPurchaseOrderByYears);
+  };
 
   // 購入申請の合計金額を計算
   // // 申請を出した時点では購入物品のチェックはfalseなので、finance_check関係なく計算
@@ -85,24 +87,28 @@ export default function PurchaseOrders(props: Props) {
 
   // 全ての購入申請の合計金額を計算
   const totalPurchaseOrderFee = useMemo(() => {
-    if (filteredPurchaseOrderViews) {
+    if (purchaseOrderViews) {
       let totalFee = 0;
-      filteredPurchaseOrderViews.map((purchaseOrderView: PurchaseOrderView) => {
+      purchaseOrderViews.map((purchaseOrderView: PurchaseOrderView) => {
         totalFee += TotalFee(purchaseOrderView.purchaseItem);
       });
       return totalFee;
     }
     return 0;
-  }, [filteredPurchaseOrderViews]);
+  }, [purchaseOrderViews]);
 
   useEffect(() => {
-    if (filteredPurchaseOrderViews) {
-      const purchaseOrderChecks = filteredPurchaseOrderViews.map((purchaseOrderView) => {
+    getPurchaseOrders();
+  }, [selectedYear]);
+
+  useEffect(() => {
+    if (purchaseOrderViews) {
+      const purchaseOrderChecks = purchaseOrderViews.map((purchaseOrderView) => {
         return purchaseOrderView.purchaseOrder.financeCheck;
       });
       setPurchaseOrderChecks(purchaseOrderChecks);
     }
-  }, [filteredPurchaseOrderViews]);
+  }, [purchaseOrderViews]);
 
   const updatePurchaseOrder = async (purchaseOrderID: number, purchaseOrder: PurchaseOrder) => {
     const url = process.env.CSR_API_URI + '/purchaseorders/' + purchaseOrderID;
@@ -160,19 +166,20 @@ export default function PurchaseOrders(props: Props) {
             <Title title={'購入申請一覧'} />
             <select
               className='w-100 '
-              defaultValue={currentYear}
-              onChange={(e) => setSelectedYear(e.target.value)}
+              defaultValue={selectedYear}
+              onChange={async (e) => {
+                setSelectedYear(e.target.value);
+              }}
             >
-              <option value='2021'>2021</option>
-              <option value='2022'>2022</option>
-              <option value='2023'>2023</option>
+              <option value='2023'>2023年度</option>
+              <option value='2024'>2024年度</option>
             </select>
             <PrimaryButton
               className='hidden md:block'
               onClick={async () => {
                 downloadFile({
                   downloadContent: await createPurchaseOrdersCsv(
-                    filteredPurchaseOrderViews,
+                    purchaseOrderViews,
                     props.expenses,
                   ),
                   fileName: `購入申請一覧(${selectedYear})_${formatYYYYMMDD(new Date())}.csv`,
@@ -213,8 +220,8 @@ export default function PurchaseOrders(props: Props) {
               </tr>
             </thead>
             <tbody className='border border-x-white-0 border-b-primary-1 border-t-white-0'>
-              {filteredPurchaseOrderViews &&
-                filteredPurchaseOrderViews.map((purchaseOrderViewItem, index) => (
+              {purchaseOrderViews &&
+                purchaseOrderViews.map((purchaseOrderViewItem, index) => (
                   <tr className='border-b' key={purchaseOrderViewItem.purchaseOrder.id}>
                     <td className='py-3'>
                       <div className='text-center text-sm text-black-600'>
@@ -324,7 +331,7 @@ export default function PurchaseOrders(props: Props) {
                     </td>
                   </tr>
                 ))}
-              {filteredPurchaseOrderViews.length > 0 && (
+              {purchaseOrderViews.length > 0 && (
                 <tr className='border-b border-primary-1'>
                   <td className='px-1 py-3' colSpan={5}>
                     <div className='flex justify-end'>
@@ -338,7 +345,7 @@ export default function PurchaseOrders(props: Props) {
                   </td>
                 </tr>
               )}
-              {!filteredPurchaseOrderViews.length && (
+              {!purchaseOrderViews.length && (
                 <tr className='border-b border-primary-1'>
                   <td className='px-1 py-3' colSpan={7}>
                     <div className='flex justify-center'>
