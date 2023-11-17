0048bf34ba1a5b68f91e1476b3aa1e4cbebbba74
TkymHrt <s231053@stn.nagaokaut.ac.jp>
2023-11-17 22:41:41 +0900

    [feat] Add Donation-Amount Data-List

---
+++ view/next-project/src/constants/donationAmount.ts
@@ -0,0 +1,18 @@
+export const DONATION_AMOUNT = [
+  {
+    id: 1,
+    name: '1000'
+  },
+  {
+    id: 2,
+    name: '2000'
+  },
+  {
+    id: 3,
+    name: '5000'
+  },
+  {
+    id: 4,
+    name: '10000'
+  },
+];
