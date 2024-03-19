909537d4a2a59d369dfa3014071bedad06b11640
hikahana <22.h.hanada.nutfes@gmail.com>
2024-03-19 11:49:22 +0000

    [fix] yearPeriodsとselectedYearの追加

--- view/next-project/src/components/purchaseorders/OpenAddModalButton.tsx
+++ view/next-project/src/components/purchaseorders/OpenAddModalButton.tsx
@@ -2,11 +2,13 @@ import React, { useState } from 'react';
 
 import PurchaseItemNumModal from './PurchaseItemNumModal';
 import { AddButton } from '@components/common';
-import { Expense } from '@type/common';
+import { Expense, YearPeriod } from '@type/common';
 
 interface Props {
   children?: React.ReactNode;
   expenses: Expense[];
+  yearPeriods: YearPeriod[];
+  selectedyear: string;
 }
 
 export default function OpenModalButton(props: Props) {
@@ -21,7 +23,7 @@ export default function OpenModalButton(props: Props) {
       >
         {props.children}
       </AddButton>
-      {isOpen && <PurchaseItemNumModal setIsOpen={setIsOpen} expenses={props.expenses} />}
+      {isOpen && <PurchaseItemNumModal setIsOpen={setIsOpen} expenses={props.expenses} yearPeriods={props.yearPeriods} selectedYear={props.selectedyear}/>}
     </>
   );
 }
