3d1425eea10b1cf2622954e08fa48481051e450d
keisuke-japan <keisuke-japan@users.noreply.github.com>
2024-03-19 10:15:13 +0000

    formatted by workflow

--- view/next-project/src/pages/purchasereports/index.tsx
+++ view/next-project/src/pages/purchasereports/index.tsx
@@ -241,8 +241,7 @@ export default function PurchaseReports(props: Props) {
           <table className='mb-5 w-max table-auto border-collapse md:w-full md:table-fixed'>
             <thead>
               <tr className='border border-x-white-0 border-b-primary-1 border-t-white-0 py-3'>
-                <th className='w-1/12 pb-2'>
-                </th>
+                <th className='w-1/12 pb-2'></th>
                 <th className='w-1/12 pb-2'>
                   <div className='text-center text-sm text-black-600'>ID</div>
                 </th>
