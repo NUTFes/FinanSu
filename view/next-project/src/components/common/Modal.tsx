11edb247930b091c1cb92ec9fb711cb44bc8414c
hikahana <22.h.hanada.nutfes@gmail.com>
2024-04-12 04:47:04 +0000

    [fix] margin-topを設けた

--- view/next-project/src/components/common/Modal.tsx
+++ view/next-project/src/components/common/Modal.tsx
@@ -8,7 +8,7 @@ interface Props {
 
 export default function Modal(props: Props) {
   const className =
-    'relative my-6 mx-auto bg-white-0 rounded-lg p-5' +
+    'relative mt-16 sm:my-6 mx-auto bg-white-0 rounded-lg p-5' +
     (props.className ? ` ${props.className}` : '');
 
   return (
