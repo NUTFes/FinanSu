49ed4d07e95a3490c85bc5169bd69750b9c610de
TkymHrt <23.h.takayama.nutfes@gmail.com>
2024-04-14 18:53:31 +0900

    [fix] モーダルの高さを制限

--- view/next-project/src/components/common/Modal.tsx
+++ view/next-project/src/components/common/Modal.tsx
@@ -14,8 +14,10 @@ export default function Modal(props: Props) {
   return (
     <>
       <div className='fixed inset-0 z-50 flex items-center justify-center overflow-y-auto overflow-x-hidden bg-black-300/50 outline-none focus:outline-none'>
-        <div className={clsx(className)}>{props.children}</div>
-      </div>
+        <div className={clsx(className)} style={{ maxHeight: '90vh', overflowY: 'auto' }}>
+          {props.children}
+        </div>
+      </div>  
     </>
   );
 }
