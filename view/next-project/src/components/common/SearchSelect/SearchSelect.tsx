2857386c892eec17719d9f590ec205ce79d3b168
Kubosaka <s223301@stn.nagaokaut.ac.jp>
2024-05-15 14:32:25 +0900

    [feat]検索機能つきドロップダウンリストの作成

---
+++ view/next-project/src/components/common/SearchSelect/SearchSelect.tsx
@@ -0,0 +1,54 @@
+import { Dispatch, useEffect, useState, SetStateAction } from 'react';
+import Select from 'react-select';
+
+interface Option {
+  value: string;
+  label: string;
+}
+
+interface SearchSelectProps {
+  options?: Option[];
+  value?: Option;
+  onChange?: (value: Option) => void;
+  setID?: Dispatch<SetStateAction<string>>;
+}
+
+const SearchSelect: React.FC<SearchSelectProps> = ({ options, value, onChange, setID }) => {
+  const [selected, setSelected] = useState<Option | null>(value || (options && options[0]) || null);
+
+  useEffect(() => {
+    setSelected(value || (options && options[0]) || null);
+  }, [options]);
+
+  return (
+    <Select
+      options={options}
+      value={selected}
+      onChange={(option, actionMeta) => {
+        switch (actionMeta.action) {
+          case 'select-option':
+            if (option) {
+              setSelected(option);
+              setID && setID(option?.value || '0');
+            }
+            break;
+          case 'remove-value':
+          case 'pop-value':
+            if (actionMeta.removedValue) {
+              setSelected(null);
+              setID && setID('');
+            }
+            break;
+          case 'clear':
+            setSelected(null);
+            setID && setID('');
+            break;
+          default:
+            break;
+        }
+      }}
+    />
+  );
+};
+
+export default SearchSelect;
