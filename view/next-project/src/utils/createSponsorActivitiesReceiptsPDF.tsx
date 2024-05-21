f7c730ea87d9aa95ac9e1f4f55fcaae3706c1fd9
TkymHrt <23.h.takayama.nutfes@gmail.com>
2024-05-21 18:30:18 +0900

    [feat] 判子を配置

--- view/next-project/src/utils/createSponsorActivitiesReceiptsPDF.tsx
+++ view/next-project/src/utils/createSponsorActivitiesReceiptsPDF.tsx
@@ -1,4 +1,15 @@
-import { Document, Page, Text, Font, View, pdf, StyleSheet, PDFViewer } from '@react-pdf/renderer';
+/* eslint-disable jsx-a11y/alt-text */
+import {
+  Document,
+  Page,
+  Text,
+  Font,
+  View,
+  pdf,
+  StyleSheet,
+  Image,
+  PDFViewer,
+} from '@react-pdf/renderer';
 import React from 'react';
 import { SponsorActivityView } from '@type/common';
 
@@ -140,6 +151,11 @@ interface MyDocumentProps {
 const MyDocument = (props: MyDocumentProps) => (
   <Document>
     <Page style={styles.page} size={{ width: 519.13, height: 241.89 }}>
+      <Image
+        fixed
+        style={{ width: '120px', position: 'absolute', top: 45, right: 20 }}
+        src={'43rd_Stamp.png'}
+      />
       <View style={styles.pageHeader}>
         <Text style={styles.header}>
           領{'  '}収{'  '}書
