5a7cbc78d440bd97200097a8c5bc12dd1ec244f5
Kubosaka <s223301@stn.nagaokaut.ac.jp>
2024-05-31 12:18:14 +0900

    [feat]ローカルでprod用を起動できるようにした

--- Makefile
+++ Makefile
@@ -12,3 +12,16 @@ down:
 seed:
 	docker compose run --rm api go mod tidy
 	docker compose run --rm api go run /app/tools/seeds/teacher_seeds.go
+local-deploy:
+	docker compose -f docker-compose.local-prod.yml build
+	docker compose -f docker-compose.local-prod.yml run --rm view npm install
+	docker compose -f docker-compose.local-prod.yml run --rm view npm run build
+	docker compose -f docker-compose.local-prod.yml up
+deploy:
+	docker compose -f docker-compose.prod.yml build
+	docker compose -f docker-compose.prod.yml run --rm view npm install
+	docker compose -f docker-compose.prod.yml run --rm view npm run build
+	docker compose -f docker-compose.prod.yml up -d
+
+
+
--- view/next-project/next.config.js
+++ view/next-project/next.config.js
@@ -4,12 +4,7 @@ const isProd = process.env.NEXT_PUBLIC_APP_ENV === 'production';
 module.exports = {
   reactStrictMode: true,
   env: {
-    // SSR_API_URI: isProd ? 'https://finansu-api.nutfes.net' : 'https://finansu-api.nutfes.net',
-    // CSR_API_URI: isProd ? 'https://finansu-api.nutfes.net' : 'https://finansu-api.nutfes.net',
-    // SSR_API_URI: isProd ? 'http://nutfes-finansu-api:1323' : 'http://nutfes-finansu-api:1323',
-    // CSR_API_URI: isProd ? 'http://localhost:1323' : 'http://localhost:1323',
-
-    SSR_API_URI: isProd ? 'https://finansu-api.nutfes.net' : 'http://nutfes-finansu-api:1323',
-    CSR_API_URI: isProd ? 'https://finansu-api.nutfes.net' : 'http://localhost:1323',
+    SSR_API_URI: process.env.NEXT_PUBLIC_SSR_API_URI,
+    CSR_API_URI: process.env.NEXT_PUBLIC_CSR_API_URI,
   },
 };
