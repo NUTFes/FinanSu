2caaf32446c5f7d895826757d78ab72aade623ed
Ryotakobayash <ryota5683804@gmail.com>
2024-06-09 16:42:36 +0900

    [add] FinanSuButton.stories.tsx

--- view/next-project/.storybook/main.ts
+++ view/next-project/.storybook/main.ts
@@ -1,7 +1,11 @@
 import type { StorybookConfig } from '@storybook/nextjs';
 
 const config: StorybookConfig = {
-  stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
+  stories: [
+    '../src/**/*.mdx',
+    '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)',
+    '../src/components/**/*.stories.@(js|jsx|ts|tsx)',
+  ],
   addons: [
     '@storybook/addon-onboarding',
     '@storybook/addon-links',
