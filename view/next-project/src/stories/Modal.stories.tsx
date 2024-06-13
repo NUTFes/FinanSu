69f0207a7810fab3e17bbadd01392289e6f2c1d0
Kubosaka <s223301@stn.nagaokaut.ac.jp>
2024-06-13 16:08:34 +0900

    [fix]不要なコメント削除

--- view/next-project/src/stories/Modal.stories.tsx
+++ view/next-project/src/stories/Modal.stories.tsx
@@ -1,19 +1,15 @@
 import { Meta } from '@storybook/react';
 import { Modal } from '@components/common';
 
-// More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
 const meta: Meta<typeof Modal> = {
   title: 'FinanSu/Modal',
   component: Modal,
   tags: ['autodocs'],
-  // More on argTypes: https://storybook.js.org/docs/api/argtypes
   argTypes: {},
-  // Use `fn` to spy on the onClick arg, which will appear in the actions panel once invoked: https://storybook.js.org/docs/essentials/actions#action-args
 };
 
 export default meta;
 
-// More on writing stories with args: https://storybook.js.org/docs/writing-stories/args
 export const Primary = {
   args: {
     className: 'm-10',
