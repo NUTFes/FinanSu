da1b1e5dcbb1b557994a7337833341af501406c3
Ryotakobayash <ryota5683804@gmail.com>
2024-06-17 22:01:53 +0900

    [add]Dropdown.stories.tsx

---
+++ view/next-project/src/stories/Dropdown.stories.tsx
@@ -0,0 +1,18 @@
+import { Meta } from '@storybook/react';
+import { Dropdown } from '@components/common';
+
+const meta: Meta<typeof Dropdown> = {
+  title: 'FinanSu/Dropdown',
+  component: Dropdown,
+  tags: ['autodocs'],
+  argTypes: {},
+};
+
+export default meta;
+
+export const Primary = {
+  args: {
+    className: 'm-10',
+    children: <h1>children</h1>,
+  },
+};
