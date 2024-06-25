a3066a648fd6cdc0a89b221826f61022fe1ba7bd
jokurebayashi <21.j.kurebayashi.nutfes@gmail.com>
2024-06-25 21:00:44 +0900

    [add]budgets/OpenAddModalButton

---
+++ view/next-project/src/stories/OpenAddModalButton.stories.tsx
@@ -0,0 +1,17 @@
+import { Meta } from '@storybook/react';
+import { OpenAddModalButton } from '@components/budgets';
+const meta: Meta<typeof OpenAddModalButton> = {
+  title: 'FinanSu/budgets/OpenAddModalButton',
+  component: OpenAddModalButton,
+  tags: ['autodocs'],
+  argTypes: {},
+};
+
+export default meta;
+
+export const Primary = {
+  args: {
+    className: 'm-10',
+    children: <h1>children</h1>,
+  },
+};
