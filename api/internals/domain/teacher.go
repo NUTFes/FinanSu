c6651c5085a7a335034926edd9e0ebbb977d339c
Kubosaka <s223301@stn.nagaokaut.ac.jp>
2024-07-01 13:48:05 +0900

    [fix]インデント追加

--- api/internals/domain/teacher.go
+++ api/internals/domain/teacher.go
@@ -5,16 +5,16 @@ import (
 )
 
 type Teacher struct {
-	ID           int       `json:"id"`
-	Name         string    `json:"name"`
-	Position     string    `json:"position"`
-	DepartmentID int       `json:"departmentID"`
-	Room         string    `json:"room"`
-	IsBlack      bool      `json:"isBlack"`
-	Remark       string    `json:"remark"`
-	IsDeleted		 bool    `json:"isDeleted"`
-	CreatedAt    time.Time `json:"createdAt"`
-	UpdatedAt    time.Time `json:"updatedAt"`
+	ID           	int       	`json:"id"`
+	Name         	string    	`json:"name"`
+	Position     	string    	`json:"position"`
+	DepartmentID 	int       	`json:"departmentID"`
+	Room         	string    	`json:"room"`
+	IsBlack      	bool      	`json:"isBlack"`
+	Remark       	string    	`json:"remark"`
+	IsDeleted		bool    	`json:"isDeleted"`
+	CreatedAt    	time.Time 	`json:"createdAt"`
+	UpdatedAt    	time.Time 	`json:"updatedAt"`
 }
 
 type DestroyTeacherIDs struct {
