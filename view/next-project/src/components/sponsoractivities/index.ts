359bb8bb92d3f57428950b37dbbcf349647ab46d
Kubosaka <s223301@stn.nagaokaut.ac.jp>
2024-07-06 01:12:10 +0900

    [fix]indexでコンポーネントのexport

---
+++ view/next-project/src/components/sponsoractivities/index.ts
@@ -0,0 +1,15 @@
+export { default as AddPdfDetailModal } from './AddPdfDetailModal';
+export { default as DeleteModal } from './DeleteModal';
+export { default as DetailModal } from './DetailModal';
+export { default as DetailPage1 } from './DetailPage1';
+export { default as DetailPage2 } from './DetailPage2';
+export { default as EditModal } from './EditModal';
+export { default as FilterModal } from './FilterModal';
+export { default as OpenAddModalButton } from './OpenAddModalButton';
+export { default as OpenAddPdfDetailModalButton } from './OpenAddPdfDetailModalButton';
+export { default as OpenDeleteModalButton } from './OpenDeleteModalButton';
+export { default as OpenEditModalButton } from './OpenEditModalButton';
+export { default as OpenPaymentDayModalButton } from './OpenPaymentDayModalButton';
+export { default as PaymentDayModal } from './PaymentDayModal';
+export { default as SponsorActivitiesAddModal } from './SponsorActivitiesAddModal';
+export { default as UploadFileModal } from './UploadFileModal';
