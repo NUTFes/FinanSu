f25555ac3fb210c10607eb033132cf6064ea799c
Ryotakobayash <ryota5683804@gmail.com>
2024-07-07 04:02:19 +0900

    [add]PurchaseReportItemNumModal.stories.tsx

---
+++ view/next-project/src/stories/purchasereports/PurchaseReportItemNumModal.stories.tsx
@@ -0,0 +1,25 @@
+import { Meta } from '@storybook/react';
+import { RecoilRoot } from 'recoil';
+import { PurchaseReportItemNumModal } from '@components/purchasereports';
+
+const meta: Meta<typeof PurchaseReportItemNumModal> = {
+  title: 'FinanSu/purchasereports/PurchaseReportItemNumModal',
+  component: PurchaseReportItemNumModal,
+  decorators: [
+    (Story) => (
+      <RecoilRoot>
+        <Story />
+      </RecoilRoot>
+    ),
+  ],
+  tags: ['autodocs'],
+  argTypes: {},
+};
+
+export default meta;
+
+export const Primary = {
+  args: {
+    children: <h1>children</h1>,
+  },
+};
