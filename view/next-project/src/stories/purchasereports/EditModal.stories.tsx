9863d166376860f71aed55a4511897c94d5e6c9c
Ryotakobayash <ryota5683804@gmail.com>
2024-07-07 03:58:46 +0900

    [add]EditModal.stories.tsx

---
+++ view/next-project/src/stories/purchasereports/EditModal.stories.tsx
@@ -0,0 +1,26 @@
+import { Meta, StoryFn } from '@storybook/react';
+import { RecoilRoot } from 'recoil';
+import { EditModal } from '@components/purchasereports';
+
+const meta: Meta<typeof EditModal> = {
+  title: 'FinanSu/purchasereports/EditModal',
+  component: EditModal,
+  decorators: [
+    (Story) => (
+      <RecoilRoot>
+        <Story />
+      </RecoilRoot>
+    ),
+  ],
+  tags: ['autodocs'],
+  argTypes: {},
+};
+
+export default meta;
+
+const Template: StoryFn<typeof EditModal> = (args) => <EditModal {...args} />;
+
+export const Primary = Template.bind({});
+Primary.args = {
+  isOpen: true,
+};
