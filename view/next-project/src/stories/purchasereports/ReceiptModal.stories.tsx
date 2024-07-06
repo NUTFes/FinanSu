c363c5369c24981429ba9d6b392dd1df83c169da
Ryotakobayash <ryota5683804@gmail.com>
2024-07-07 04:02:41 +0900

    [add]ReceiptModal.stories.tsx

---
+++ view/next-project/src/stories/purchasereports/ReceiptModal.stories.tsx
@@ -0,0 +1,18 @@
+import { Meta } from '@storybook/react';
+import { ReceiptModal } from '@components/purchasereports';
+
+const meta: Meta<typeof ReceiptModal> = {
+  title: 'FinanSu/purchasereports/ReceiptModal',
+  component: ReceiptModal,
+  tags: ['autodocs'],
+  argTypes: {},
+};
+
+export default meta;
+
+export const Primary = {
+  args: {
+    children: <h1>children</h1>,
+    purchaseReportId: 1,
+  },
+};
