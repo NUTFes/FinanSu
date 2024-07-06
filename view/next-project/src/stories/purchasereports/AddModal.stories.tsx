8be395e644dbc10fe0e6d9fb3d7992faff7eea81
Ryotakobayash <ryota5683804@gmail.com>
2024-07-07 03:57:35 +0900

    AddModal.stories.tsx

---
+++ view/next-project/src/stories/purchasereports/AddModal.stories.tsx
@@ -0,0 +1,18 @@
+import { Meta } from '@storybook/react';
+import { AddModal } from '@components/purchasereports';
+
+const meta: Meta<typeof AddModal> = {
+  title: 'FinanSu/purchasereports/AddModal',
+  component: AddModal,
+  tags: ['autodocs'],
+  argTypes: {},
+};
+
+export default meta;
+
+export const Primary = {
+  args: {
+    className: 'm-10',
+    children: <h1>children</h1>,
+  },
+};
