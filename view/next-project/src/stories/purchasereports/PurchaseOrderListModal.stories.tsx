5fb78e3645aba871f64872102b70eb9a71c3b82a
Ryotakobayash <ryota5683804@gmail.com>
2024-07-07 03:59:55 +0900

    PurchaseOrderListModal.stories.tsx

---
+++ view/next-project/src/stories/purchasereports/PurchaseOrderListModal.stories.tsx
@@ -0,0 +1,17 @@
+import { Meta } from '@storybook/react';
+import { PurchaseOrderListModal } from '@components/purchasereports';
+
+const meta: Meta<typeof PurchaseOrderListModal> = {
+  title: 'FinanSu/purchasereports/PurchaseOrderListModal',
+  component: PurchaseOrderListModal,
+  tags: ['autodocs'],
+  argTypes: {},
+};
+
+export default meta;
+
+export const Primary = {
+  args: {
+    children: <h1>children</h1>,
+  },
+};
