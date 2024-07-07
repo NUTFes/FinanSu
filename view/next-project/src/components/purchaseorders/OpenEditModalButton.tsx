f35bef18ba4ea2ccbb65c134dca26592265d943c
TkymHrt <23.h.takayama.nutfes@gmail.com>
2024-07-07 15:33:22 +0900

    [feat] 選択モーダルを追加

--- view/next-project/src/components/purchaseorders/OpenEditModalButton.tsx
+++ view/next-project/src/components/purchaseorders/OpenEditModalButton.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 
-import { EditButton } from '@components/common';
+import { DetailEditModal } from './DetailEditModal';
+import { CloseButton, EditButton, Modal, PrimaryButton } from '@components/common';
 import EditModal from '@components/purchaseorders/EditModal';
 import { PurchaseItem } from '@type/common';
 
@@ -11,20 +12,54 @@ interface Props {
   isDisabled: boolean;
 }
 
+const InitialModal: React.FC<{ setStep: (step: string) => void; closeModal: () => void }> = ({
+  setStep,
+  closeModal,
+}) => (
+  <Modal className='md:w-1/2'>
+    <div className='ml-auto w-fit'>
+      <CloseButton onClick={closeModal} />
+    </div>
+    <div className='mx-auto mb-6 w-fit text-xl text-black-600'>
+      <p>購入申請の修正</p>
+    </div>
+    <div className='flex justify-center gap-2 pb-4'>
+      <PrimaryButton onClick={() => setStep('editDetails')}>局と期限日を編集</PrimaryButton>
+      <PrimaryButton onClick={() => setStep('editPurchases')}>購入物品を編集</PrimaryButton>
+    </div>
+  </Modal>
+);
+
 const OpenEditModalButton: React.FC<Props> = (props) => {
-  const [isOpen, setIsOpen] = useState(false);
-  const onOpen = () => {
-    setIsOpen(true);
+  const [step, setStep] = useState<string | null>(null);
+
+  const onOpenInitial = () => {
+    setStep('initial');
   };
+
+  const closeModal = () => {
+    setStep(null);
+  };
+
   return (
     <>
-      <EditButton onClick={onOpen} isDisabled={props.isDisabled} />
-      {isOpen && (
+      <EditButton onClick={onOpenInitial} isDisabled={props.isDisabled} />
+      {step === 'initial' && <InitialModal setStep={setStep} closeModal={closeModal} />}
+      {step === 'editDetails' && (
+        <DetailEditModal
+          purchaseReportId={props.id}
+          isOpen={true}
+          setIsOpen={closeModal}
+          onOpenInitial={onOpenInitial}
+        />
+      )}
+      {step === 'editPurchases' && (
         <EditModal
           purchaseOrderId={props.id}
-          isOpen={isOpen}
-          setIsOpen={setIsOpen}
+          isOpen={true}
+          setIsOpen={closeModal}
           purchaseItems={props.purchaseItems}
+          onOpenInitial={onOpenInitial}
         />
       )}
     </>
