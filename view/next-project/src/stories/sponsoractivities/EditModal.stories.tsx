2d8d71782b005fe04ef9a7c1c44741298b02df3f
Ryotakobayash <ryota5683804@gmail.com>
2024-07-24 16:36:15 +0900

    [add]EditModal.stories.tsx

---
+++ view/next-project/src/stories/sponsoractivities/EditModal.stories.tsx
@@ -0,0 +1,34 @@
+import { Meta, StoryFn } from '@storybook/react';
+import {
+  USER,
+  SPONSOR_ACTIVITY,
+  SPONSOR_STYLE,
+  SPONSOR,
+  ACTIVITY_STYLE,
+  YEAEPERIOD,
+} from '../constants';
+import { EditModal } from '@components/sponsoractivities';
+
+const meta: Meta<typeof EditModal> = {
+  title: 'FinanSu/sponsoractivities/EditModal',
+  component: EditModal,
+  tags: ['autodocs'],
+  argTypes: {},
+};
+
+export default meta;
+
+const Template: StoryFn<typeof EditModal> = (args) => <EditModal {...args} />;
+
+export const Primary = Template.bind({});
+Primary.args = {
+  sponsorActivityId: '1000',
+  sponsorActivity: SPONSOR_ACTIVITY,
+  sponsorStyles: [SPONSOR_STYLE],
+  sponsors: [SPONSOR],
+  users: [USER],
+  sponsorStyleDetails: [ACTIVITY_STYLE],
+  activityStyles: [ACTIVITY_STYLE],
+  year: '2024',
+  yearPeriods: YEAEPERIOD,
+};
