140535691e565ac389fe190b164088d63061af90
Ryotakobayash <ryota5683804@gmail.com>
2024-07-24 16:34:57 +0900

    [add]OpenDeleteModalButton.stories.tsx

---
+++ view/next-project/src/stories/sponsoractivities/OpenDeleteModalButton.stories.tsx
@@ -0,0 +1,18 @@
+import { Meta } from '@storybook/react';
+import { OpenDeleteModalButton } from '@components/sponsoractivities';
+
+const meta: Meta<typeof OpenDeleteModalButton> = {
+  title: 'FinanSu/sponsoractivities/OpenDeleteModalButton',
+  component: OpenDeleteModalButton,
+  tags: ['autodocs'],
+  argTypes: {},
+};
+
+export default meta;
+
+export const Primary = {
+  args: {
+    className: 'm-10',
+    children: <h1>children</h1>,
+  },
+};
