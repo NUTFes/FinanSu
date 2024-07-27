2ea7dfb23c8fdd2dbe973d35db8bf57431562dde
Ryotakobayash <ryota5683804@gmail.com>
2024-07-27 17:49:21 +0900

    [add]SignInView,SignUpView #860

---
+++ view/next-project/src/stories/auth/SignInView.stories.tsx
@@ -0,0 +1,24 @@
+import { Meta, StoryFn } from '@storybook/react';
+import { RecoilRoot } from 'recoil';
+import { SignInView } from '@components/auth';
+
+const meta: Meta<typeof SignInView> = {
+  title: 'FinanSu/auth/SignInView',
+  component: SignInView,
+  decorators: [
+    (Story) => (
+      <RecoilRoot>
+        <Story />
+      </RecoilRoot>
+    ),
+  ],
+  tags: ['autodocs'],
+  argTypes: {},
+};
+
+export default meta;
+
+const Template: StoryFn<typeof SignInView> = () => <SignInView />;
+
+export const Primary = Template.bind({});
+Primary.args = {};
