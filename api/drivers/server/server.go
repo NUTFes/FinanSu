af2d0b95ce8c680bdb9713b3aca28f749627d078
hikahana <22.h.hanada.nutfes@gmail.com>
2024-12-23 17:05:22 +0000

    [fix] swagger切り離しのためcors修正

--- api/drivers/server/server.go
+++ api/drivers/server/server.go
@@ -1,13 +1,12 @@
 package server
 
 import (
-	_ "github.com/NUTFes/FinanSu/api/docs"
+	"net/http"
+	"os"
+
 	"github.com/NUTFes/FinanSu/api/router"
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
-	echoSwagger "github.com/swaggo/echo-swagger"
-	"net/http"
-	"os"
 )
 
 func RunServer(router router.Router) {
@@ -28,15 +27,12 @@ func RunServer(router router.Router) {
 
 	// CORS対策
 	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
-		AllowOrigins: []string{"http://localhost:3000", "127.0.0.1:3000", "http://localhost:3001", "127.0.0.1:3001", "https://finansu.nutfes.net"}, // ドメイン
+		AllowOrigins: []string{"http://localhost:3000", "127.0.0.1:3000", "http://localhost:3001", "127.0.0.1:3001", "http://localhost:8000", "127.0.0.1:8000", "https://finansu.nutfes.net"}, // ドメイン
 		AllowMethods: []string{http.MethodGet, http.MethodPost, http.MethodPut, http.MethodDelete},
 	}))
 
 	// ルーティング
 	router.ProvideRouter(e)
-
-	// swagger
-	e.GET("/swagger/*", echoSwagger.WrapHandler)
 
 	// サーバー起動
 	e.Start(":1323")
