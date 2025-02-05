c8dfb6bd6ea0da5c7021060748569d7b7eed1c84
Kubosaka <s223301@stn.nagaokaut.ac.jp>
2025-02-05 21:55:10 +0900

    不要部分削除

--- api/drivers/server/server.go
+++ api/drivers/server/server.go
@@ -6,41 +6,11 @@ import (
 
 	"github.com/NUTFes/FinanSu/api/externals/handler"
 	"github.com/NUTFes/FinanSu/api/generated"
-	"github.com/NUTFes/FinanSu/api/router"
 	echo "github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 )
 
-func RunServer(router router.Router) *echo.Echo {
-	// echoのインスタンス
-	e := echo.New()
-
-	// Middleware
-	e.Use(middleware.Recover())
-
-	// log
-	logger := middleware.LoggerWithConfig(
-		middleware.LoggerConfig{
-			Format: logFormat(),
-			Output: os.Stdout,
-		},
-	)
-	e.Use(logger)
-
-	// CORS対策
-	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
-		AllowOrigins: []string{"http://localhost:3000", "127.0.0.1:3000", "http://localhost:3001", "127.0.0.1:3001", "http://localhost:8000", "127.0.0.1:8000", "https://finansu.nutfes.net"}, // ドメイン
-		AllowMethods: []string{http.MethodGet, http.MethodPost, http.MethodPut, http.MethodDelete},
-	}))
-
-	// ルーティング
-	router.ProvideRouter(e)
-
-	// サーバー起動
-	return e
-}
-
-func RunServer2(server *handler.Handler) *echo.Echo {
+func RunServer(server *handler.Handler) *echo.Echo {
 	// echoのインスタンス
 	e := echo.New()
 
