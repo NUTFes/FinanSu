ca9e61ad0875a90dd98068b98686bb123454c033
TkymHrt <23.h.takayama.nutfes@gmail.com>
2025-03-01 23:23:41 +0900

    [feat] 編集モードとエラー表示を追加

--- view/next-project/src/components/create_purchase_report/FileUploadField.tsx
+++ view/next-project/src/components/create_purchase_report/FileUploadField.tsx
@@ -4,21 +4,25 @@ import { FaExclamationCircle } from 'react-icons/fa';
 import { GoPlus } from 'react-icons/go';
 
 interface FileUploadFieldProps {
-  isFromReport: boolean;
-  receiptFile: File | null;
+  isEditMode: boolean;
+  uploadedFile: File | null;
   fileInputRef: RefObject<HTMLInputElement>;
   handleFileChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  validationError?: string;
 }
 
 const FileUploadField: React.FC<FileUploadFieldProps> = ({
-  isFromReport,
-  receiptFile,
+  isEditMode,
+  uploadedFile,
   fileInputRef,
   handleFileChange,
+  validationError,
 }) => {
+  const handleFileUploadClick = () => fileInputRef.current?.click();
+
   return (
-    <FormControl id='receipt-upload' isRequired={!isFromReport}>
-      <FormLabel>{isFromReport ? '領収書（レシート）の変更' : '領収書（レシート）'}</FormLabel>
+    <FormControl id='receipt-upload' isRequired={!isEditMode} isInvalid={!!validationError}>
+      <FormLabel>{isEditMode ? '領収書（レシート）の変更' : '領収書（レシート）'}</FormLabel>
       <input
         type='file'
         accept='image/jpeg,image/png,image/gif,application/pdf'
@@ -26,12 +30,12 @@ const FileUploadField: React.FC<FileUploadFieldProps> = ({
         onChange={handleFileChange}
         className='hidden'
       />
-      {receiptFile ? (
+      {uploadedFile ? (
         <div
           className='border-gray-200 hover:bg-gray-300 flex min-h-[40px] cursor-pointer items-center gap-2 rounded-md border bg-[#E7E7E7] p-2 transition-colors'
-          onClick={() => fileInputRef.current?.click()}
+          onClick={handleFileUploadClick}
         >
-          <Text className='ml-2 min-w-0 flex-1 truncate'>{receiptFile.name}</Text>
+          <Text className='ml-2 min-w-0 flex-1 truncate'>{uploadedFile.name}</Text>
           <Text className='text-gray-500 mr-2 text-sm'>クリックして変更</Text>
         </div>
       ) : (
@@ -39,21 +43,31 @@ const FileUploadField: React.FC<FileUploadFieldProps> = ({
           className='w-full bg-[#E7E7E7]'
           colorScheme='gray'
           variant='outline'
-          onClick={() => fileInputRef.current?.click()}
+          onClick={handleFileUploadClick}
         >
-          <span className='flex w-full items-center gap-2 font-normal'>
-            <GoPlus />
-            {isFromReport ? '領収書（レシート）を変更' : '領収書（レシート）をアップロード'}
+          <span className='flex w-full items-center justify-center gap-1 sm:gap-2 font-normal'>
+            <GoPlus className="text-sm sm:text-base pt-[1px]" />
+            <Text className="text-xs sm:text-sm md:text-base">
+              {isEditMode ? '領収書（レシート）を変更' : '領収書（レシート）をアップロード'}
+            </Text>
           </span>
         </Button>
       )}
-      {!receiptFile && !isFromReport && (
+      {validationError ? (
         <Box className='mt-6 flex items-center justify-center gap-2'>
-          <FaExclamationCircle color='#B91C1C' />
-          <Text className='text-sm text-[#B91C1C]'>
-            領収書（レシート）をアップロードしてください
-          </Text>
+          <FaExclamationCircle color='#B91C1C' className='pt-[1px]' />
+          <Text className='text-sm text-[#B91C1C]'>{validationError}</Text>
         </Box>
+      ) : (
+        !uploadedFile &&
+        !isEditMode && (
+          <Box className='mt-6 flex items-center justify-center gap-2'>
+            <FaExclamationCircle color='#B91C1C' />
+            <Text className='text-sm text-[#B91C1C]'>
+              領収書（レシート）をアップロードしてください
+            </Text>
+          </Box>
+        )
       )}
     </FormControl>
   );
