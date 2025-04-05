813450652cc351f3d8afd1554f92579a05ea2226
Kubosaka <s223301@stn.nagaokaut.ac.jp>
2025-04-05 15:04:24 +0900

    ddl修正

--- mysql/db/11_income_expenditure_managements copy.sql
+++ mysql/db/11_income_expenditure_managements copy.sql
@@ -4,12 +4,21 @@ CREATE TABLE income_expenditure_managements (
     id INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
     content VARCHAR(255) NOT NULL,
     amount INT(10) NOT NULL,
-    -- paid_by VARCHAR(255) NOT NULL, // 確認中
     log_category ENUM("income", "expenditure", "sponsor's income") NOT NULL,
     year_id INT(10) UNSIGNED NOT NULL,
+    receive_option ENUM("transfer", "hand"),
     is_checked BOOLEAN NOT NULL DEFAULT FALSE,
     created_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
     updated_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
     PRIMARY KEY (id),
     FOREIGN KEY year_id_foreign_key (year_id) REFERENCES years (id) ON DELETE CASCADE
 );
+
+USE finansu_db;
+
+-- シードデータ挿入
+INSERT INTO income_expenditure_managements (content, amount, log_category, year_id, receive_option, is_checked, created_at, updated_at)
+VALUES
+    ("雑収入", 50000, "income", 3, "hand", FALSE, NOW(), NOW()), -- 雑収入
+    ("情報局", 20000, "expenditure", 3, NULL, FALSE, NOW(), NOW()), -- 情報局支出
+    ("企業協賛", 100000, "sponsor's income", 3, "transfer", FALSE, NOW(), NOW()); -- 企業協賛
