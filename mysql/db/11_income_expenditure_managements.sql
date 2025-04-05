0e9189f767f3093c8b86d3acfe668efdbe488167
Kubosaka <s223301@stn.nagaokaut.ac.jp>
2025-04-05 15:24:48 +0900

    ddl修正

--- mysql/db/11_income_expenditure_managements.sql
+++ mysql/db/11_income_expenditure_managements.sql
@@ -2,11 +2,10 @@ USE finansu_db;
 
 CREATE TABLE income_expenditure_managements (
     id INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
-    content VARCHAR(255) NOT NULL,
     amount INT(10) NOT NULL,
     log_category ENUM("income", "expenditure", "sponsor's income") NOT NULL,
     year_id INT(10) UNSIGNED NOT NULL,
-    receive_option ENUM("transfer", "hand"),
+    receive_option ENUM("transfer", "hand"), -- expenditureはNULLを許可する
     is_checked BOOLEAN NOT NULL DEFAULT FALSE,
     created_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
     updated_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
@@ -16,9 +15,9 @@ CREATE TABLE income_expenditure_managements (
 
 USE finansu_db;
 
--- シードデータ挿入
-INSERT INTO income_expenditure_managements (content, amount, log_category, year_id, receive_option, is_checked, created_at, updated_at)
+-- income_expenditure_managements のシードデータ挿入
+INSERT INTO income_expenditure_managements (amount, log_category, year_id, receive_option, is_checked, created_at, updated_at)
 VALUES
-    ("雑収入", 50000, "income", 3, "hand", FALSE, NOW(), NOW()), -- 雑収入
-    ("情報局", 20000, "expenditure", 3, NULL, FALSE, NOW(), NOW()), -- 情報局支出
-    ("企業協賛", 100000, "sponsor's income", 3, "transfer", FALSE, NOW(), NOW()); -- 企業協賛
+    (50000, "income", 3, "hand", FALSE, NOW(), NOW()), -- 雑収入
+    (20000, "expenditure", 3, NULL, FALSE, NOW(), NOW()), -- 情報局支出
+    (100000, "sponsor's income", 3, "transfer", FALSE, NOW(), NOW()); -- 企業協賛
