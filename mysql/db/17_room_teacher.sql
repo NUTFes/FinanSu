c4a803ee5549e5a69eb7f2ffda60b29e329d54dd
hikahana <22.h.hanada.nutfes@gmail.com>
2025-04-09 03:03:11 +0900

    feat: roomとteacherの中間テーブル作成

---
+++ mysql/db/17_room_teacher.sql
@@ -0,0 +1,15 @@
+use finansu_db
+
+-- roomsとteachersの中間テーブル
+CREATE TABLE room_teacher (
+  id INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+  room_id INT(10) unsigned not null,
+  teacher_id INT(10) unsigned not null,
+  created_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  updated_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (id),
+  FOREIGN KEY (room_id) REFERENCES rooms(id),
+  FOREIGN KEY (teacher_id) REFERENCES teachers(id)
+);
+
+INSERT INTO room_teacher (room_id, teacher_id) VALUES (1, 1)
