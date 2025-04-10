06cd49309ba97f434a3599b609a3affbf7cd1cf7
hikahana <22.h.hanada.nutfes@gmail.com>
2025-04-10 13:52:58 +0900

    fix: シードデータ修正

--- mysql/db/39_building_units.sql
+++ mysql/db/39_building_units.sql
@@ -9,7 +9,20 @@ CREATE TABLE building_units(
   PRIMARY KEY (id),
   FOREIGN KEY (building_id) REFERENCES buildings(id)
 );
-
-INSERT INTO building_units (id, unit_number) VALUES (1, "1号棟");
-INSERT INTO building_units (id, unit_number) VALUES (2, "2号棟");
-INSERT INTO building_units (id, unit_number) VALUES (3, "3号棟");
+INSERT INTO building_units (building_id, unit_number) VALUES (1, "1号棟");
+INSERT INTO building_units (building_id, unit_number) VALUES (1, "2号棟");
+INSERT INTO building_units (building_id, unit_number) VALUES (1, "3号棟");
+INSERT INTO building_units (building_id, unit_number) VALUES (2, "1号棟");
+INSERT INTO building_units (building_id, unit_number) VALUES (2, "2号棟");
+INSERT INTO building_units (building_id, unit_number) VALUES (3, "1号棟");
+INSERT INTO building_units (building_id, unit_number) VALUES (4, "1号棟");
+INSERT INTO building_units (building_id, unit_number) VALUES (5, "1号棟");
+INSERT INTO building_units (building_id, unit_number) VALUES (5, "2号棟");
+INSERT INTO building_units (building_id, unit_number) VALUES (6, "1号棟");
+INSERT INTO building_units (building_id, unit_number) VALUES (7, "1号棟");
+INSERT INTO building_units (building_id, unit_number) VALUES (8, "1号棟");
+INSERT INTO building_units (building_id, unit_number) VALUES (9, "1号棟");
+INSERT INTO building_units (building_id, unit_number) VALUES (10, "1号棟");
+INSERT INTO building_units (building_id, unit_number) VALUES (11, "1号棟");
+INSERT INTO building_units (building_id, unit_number) VALUES (12, "1号棟");
+INSERT INTO building_units (building_id, unit_number) VALUES (13, "1号棟");
