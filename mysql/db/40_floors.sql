151377fd5a92c4936f11c68e7bf082ba87bb2c99
hikahana <22.h.hanada.nutfes@gmail.com>
2025-04-12 00:11:17 +0900

    [fix] not null追加

--- mysql/db/40_floors.sql
+++ mysql/db/40_floors.sql
@@ -2,7 +2,7 @@ use finansu_db
 
 CREATE TABLE floors(
   id int(10) unsigned not null auto_increment,
-  building_unit_id int(10) unsigned,
+  building_unit_id int(10) unsigned not null,
   floor_number varchar(255) not null,
   created_at datetime not null default current_timestamp,
   updated_at datetime not null default current_timestamp on update current_timestamp,
