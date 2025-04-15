d0e4aa435b2d0c923a990eb10b607cf0d4a2334e
Kubosaka <s223301@stn.nagaokaut.ac.jp>
2025-04-15 21:33:04 +0900

    sql整形

--- mysql/db/18_departments.sql
+++ mysql/db/18_departments.sql
@@ -1,23 +1,27 @@
 use finansu_db;
 
-CREATE TABLE departments (
-  id int(10) unsigned not null auto_increment,
-  name varchar(255),
-  created_at datetime not null default current_timestamp,
-  updated_at datetime not null default current_timestamp on update current_timestamp,
-  PRIMARY KEY (id)
-);
+CREATE TABLE
+  departments (
+    id int(10) unsigned not null auto_increment,
+    name varchar(255),
+    created_at datetime not null default current_timestamp,
+    updated_at datetime not null default current_timestamp on update current_timestamp,
+    PRIMARY KEY (id)
+  );
 
-INSERT into departments (name) values ('電気電子情報');
-INSERT into departments (name) values ('物質生物');
-INSERT into departments (name) values ('機械創造');
-INSERT into departments (name) values ('環境社会基盤');
-INSERT into departments (name) values ('情報・経営システム');
-INSERT into departments (name) values ('基盤共通教育');
-INSERT into departments (name) values ('原子力システム安全');
-INSERT into departments (name) values ('技術科学イノベーション');
-INSERT into departments (name) values ('システム安全');
-INSERT into departments (name) values ('技術支援');
-INSERT into departments (name) values ('その他');
-INSERT into departments (name) values ('学長・事務');
-INSERT into departments (name) values ('FL');
+INSERT INTO
+  departments (name)
+VALUES
+  ('電気電子情報'),
+  ('物質生物'),
+  ('機械創造'),
+  ('環境社会基盤'),
+  ('情報・経営システム'),
+  ('基盤共通教育'),
+  ('原子力システム安全'),
+  ('技術科学イノベーション'),
+  ('システム安全'),
+  ('技術支援'),
+  ('その他'),
+  ('学長・事務'),
+  ('FL');
--- mysql/db/23_purcahse_items.sql
+++ mysql/db/23_purcahse_items.sql
@@ -1,27 +1,31 @@
 use finansu_db;
 
-CREATE TABLE purchase_items (
-  id int(10) unsigned not null auto_increment,
-  item varchar(255),
-  price int(10),
-  quantity int(10),
-  detail varchar(255),
-  url varchar(255),
-  purchase_order_id int(10),
-  finance_check boolean,
-  created_at datetime not null default current_timestamp,
-  updated_at datetime not null default current_timestamp on update current_timestamp,
-  PRIMARY KEY (id)
-);
+CREATE TABLE
+  purchase_items (
+    id int(10) unsigned not null auto_increment,
+    item varchar(255),
+    price int(10),
+    quantity int(10),
+    detail varchar(255),
+    url varchar(255),
+    purchase_order_id int(10),
+    finance_check boolean,
+    created_at datetime not null default current_timestamp,
+    updated_at datetime not null default current_timestamp on update current_timestamp,
+    PRIMARY KEY (id)
+  );
 
-INSERT into purchase_items (item, price, quantity, detail, url, purchase_order_id, finance_check )values ('ビンゴ景品', 10000, 1, 'test-detail', 'https://nutfes.net', 1, true);
-INSERT into purchase_items (item, price, quantity, detail, url, purchase_order_id, finance_check )values ('ヒーロースーツ', 20000, 2, 'test-detail2', 'https://nutfes.net', 1, false);
-INSERT into purchase_items (item, price, quantity, detail, url, purchase_order_id, finance_check )values ('ビンゴ用品', 30000, 3, 'test-detail3', 'https://nutfes.net', 2, true); 
-INSERT into purchase_items (item, price, quantity, detail, url, purchase_order_id, finance_check )values ('マイク', 10000, 1, 'test-detail', 'https://nutfes.net', 2, true);
-INSERT into purchase_items (item, price, quantity, detail, url, purchase_order_id, finance_check )values ('机', 20000, 2, 'test-detail2', 'https://nutfes.net', 3, true);
-INSERT into purchase_items (item, price, quantity, detail, url, purchase_order_id, finance_check )values ('椅子', 30000, 3, 'test-detail3', 'https://nutfes.net', 3,  false);
-INSERT into purchase_items (item, price, quantity, detail, url, purchase_order_id, finance_check )values ('酒', 10000, 10, 'test-detail', 'https://nutfes.net', 4, true);
-INSERT into purchase_items (item, price, quantity, detail, url, purchase_order_id, finance_check )values ('封筒', 100, 100, 'test-detail2', 'https://nutfes.net', 5, false);
-INSERT into purchase_items (item, price, quantity, detail, url, purchase_order_id, finance_check )values ('領収書', 100, 50, 'test-detail3', 'https://nutfes.net', 6, true);
-INSERT into purchase_items (item, price, quantity, detail, url, purchase_order_id, finance_check )values ('封筒', 100, 100, 'test-detail2', 'https://nutfes.net', 8, false);
-INSERT into purchase_items (item, price, quantity, detail, url, purchase_order_id, finance_check )values ('領収書', 100, 50, 'test-detail3', 'https://nutfes.net', 7, true);
+INSERT INTO
+  purchase_items (item, price, quantity, detail, url, purchase_order_id, finance_check)
+VALUES
+  ('ビンゴ景品', 10000, 1, 'test-detail', 'https://nutfes.net', 1, true),
+  ('ヒーロースーツ', 20000, 2, 'test-detail2', 'https://nutfes.net', 1, false),
+  ('ビンゴ用品', 30000, 3, 'test-detail3', 'https://nutfes.net', 2, true),
+  ('マイク', 10000, 1, 'test-detail', 'https://nutfes.net', 2, true),
+  ('机', 20000, 2, 'test-detail2', 'https://nutfes.net', 3, true),
+  ('椅子', 30000, 3, 'test-detail3', 'https://nutfes.net', 3, false),
+  ('酒', 10000, 10, 'test-detail', 'https://nutfes.net', 4, true),
+  ('封筒', 100, 100, 'test-detail2', 'https://nutfes.net', 5, false),
+  ('領収書', 100, 50, 'test-detail3', 'https://nutfes.net', 6, true),
+  ('封筒', 100, 100, 'test-detail2', 'https://nutfes.net', 8, false),
+  ('領収書', 100, 50, 'test-detail3', 'https://nutfes.net', 7, true);
