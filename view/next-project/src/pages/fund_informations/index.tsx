11017e29b9b6fca15b32c4477cefffb90bb2a39d
TkymHrt <TkymHrt@users.noreply.github.com>
2025-04-21 19:20:05 +0000

    formatted by workflow

--- view/next-project/src/pages/fund_informations/index.tsx
+++ view/next-project/src/pages/fund_informations/index.tsx
@@ -135,7 +135,7 @@ const FundInformationHeader = ({ totalBalance }: { totalBalance: number }) => (
   <div className='flex flex-col gap-4 md:flex-row md:items-center md:justify-between'>
     <Title title={'収支管理'} />
     <Title className='gap-0 text-xl'>
-        残高<span className='ml-1'>{totalBalance.toLocaleString()}</span>
+      残高<span className='ml-1'>{totalBalance.toLocaleString()}</span>
     </Title>
     <OpenAddModalButton teachers={[]} departments={[]} users={[]} currentYear={''}>
       収入報告
