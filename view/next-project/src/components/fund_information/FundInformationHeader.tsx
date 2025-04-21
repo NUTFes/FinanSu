0a1ff595c33405a10dd297cee98e7f9875169f70
TkymHrt <23.h.takayama.nutfes@gmail.com>
2025-04-22 03:33:23 +0900

    [fix] 残高表示のCSSを修正

--- view/next-project/src/components/fund_information/FundInformationHeader.tsx
+++ view/next-project/src/components/fund_information/FundInformationHeader.tsx
@@ -16,7 +16,9 @@ const FundInformationHeader: React.FC<FundInformationHeaderProps> = ({ totalBala
   return (
     <div className='flex flex-col gap-4 md:flex-row md:items-center md:justify-between'>
       <Title title={'収支管理'} />
-      <Title className='text-xl'>残高 {totalBalance.toLocaleString()}</Title>
+      <Title className='gap-0 text-xl'>
+        残高<span className='ml-1'>{totalBalance.toLocaleString()}</span>
+      </Title>
       <AddButton onClick={handleCreateClick}>収入報告</AddButton>
     </div>
   );
