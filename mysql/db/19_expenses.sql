33f069582d5b120cdef73a982c71fc5a74a6e34f
KazumaSun <satoma.2001.0920@gmail.com>
2025-05-10 07:35:27 +0000

    mysqlの修正

--- mysql/db/19_expenses.sql
+++ mysql/db/19_expenses.sql
@@ -26,94 +26,66 @@ VALUES
 	("備品整備準備費", 2),
 	("翌年度繰越金", 2);
 
--- 終端文字の変更
-DELIMITER / /
--- ストアドプロシージャ作成
-CREATE PROCEDURE updateExpense () BEGIN
--- 1 テンポラリテーブル作成tmp,tmp
-CREATE TEMPORARY TABLE
-	tmp (
-		id int(10) NOT NULL,
-		totalPrice int(10),
-		purchase_reports_id int(10),
-		addition int(10),
-		discount int(10),
-		expense_id int(10),
-		finance_check boolean,
-		PRIMARY KEY (`id`)
-	);
+-- -- ストアドプロシージャ作成
+-- CREATE PROCEDURE updateExpense()
+-- BEGIN
+--     -- テンポラリテーブル作成
+--     CREATE TEMPORARY TABLE tmp (
+--         id INT NOT NULL,
+--         totalPrice INT,
+--         purchase_reports_id INT,
+--         addition INT,
+--         discount INT,
+--         expense_id INT,
+--         finance_check BOOLEAN,
+--         PRIMARY KEY (id)
+--     );
 
-CREATE TEMPORARY TABLE
-	tmp2 (id int(10) NOT NULL, totalPrice int(10) NOT NULL, PRIMARY KEY (`id`));
+--     CREATE TEMPORARY TABLE tmp2 (
+--         id INT NOT NULL,
+--         totalPrice INT NOT NULL,
+--         PRIMARY KEY (id)
+--     );
 
--- 2 mpにpurchase_itemsのfinansu_checkがtrueのものをpurchase_orderごとに和を入れる
-INSERT INTO
-	tmp (id, totalPrice)
-SELECT
-	pi.purchase_order_id,
-	SUM(pi.price * pi.quantity)
-FROM
-	purchase_items pi
-WHERE
-	pi.finance_check IS true
-GROUP BY
-	pi.purchase_order_id;
+--     -- データ操作
+--     INSERT INTO tmp (id, totalPrice)
+--     SELECT pi.purchase_order_id, SUM(pi.price * pi.quantity)
+--     FROM purchase_items pi
+--     WHERE pi.finance_check IS TRUE
+--     GROUP BY pi.purchase_order_id;
 
--- 3 tmpにpurhchase_reportsのデータを入れる
-UPDATE tmp
-INNER JOIN purchase_reports pr ON tmp.id = pr.purchase_order_id
-SET
-	tmp.purchase_reports_id = pr.id,
-	tmp.addition = pr.addition,
-	tmp.discount = pr.discount,
-	tmp.finance_check = pr.finance_check;
+--     UPDATE tmp
+--     INNER JOIN purchase_reports pr ON tmp.id = pr.purchase_order_id
+--     SET tmp.purchase_reports_id = pr.id,
+--         tmp.addition = pr.addition,
+--         tmp.discount = pr.discount,
+--         tmp.finance_check = pr.finance_check;
 
--- tmpにpurchaser_ordersのexpense_idを入れる
-UPDATE tmp
-INNER JOIN purchase_orders po ON tmp.id = po.id
-SET
-	tmp.expense_id = po.expense_id
-WHERE
-	po.finance_check IS true;
+--     UPDATE tmp
+--     INNER JOIN purchase_orders po ON tmp.id = po.id
+--     SET tmp.expense_id = po.expense_id
+--     WHERE po.finance_check IS TRUE;
 
--- expense_idがNULLのレコードを削除する
-DELETE FROM tmp
-WHERE
-	expense_id IS NULL;
+--     DELETE FROM tmp
+--     WHERE expense_id IS NULL;
 
--- tmpのデータをexpense_idごとにまとめて総和を求める、データをtmp2に入れる
-INSERT INTO
-	tmp2 (id, totalPrice)
-SELECT
-	tmp.expense_id,
-	SUM(tmp.totalPrice + tmp.addition - tmp.discount)
-FROM
-	tmp
-WHERE
-	tmp.finance_check IS true
-GROUP BY
-	tmp.expense_id;
+--     INSERT INTO tmp2 (id, totalPrice)
+--     SELECT tmp.expense_id,
+--            SUM(tmp.totalPrice + tmp.addition - tmp.discount)
+--     FROM tmp
+--     WHERE tmp.finance_check IS TRUE
+--     GROUP BY tmp.expense_id;
 
--- tmp2のデータをexpeneseに入れる
-UPDATE expenses
-INNER JOIN tmp2 ON expenses.id = tmp2.id
-SET
-	expenses.totalPrice = tmp2.totalPrice;
+--     UPDATE expenses
+--     INNER JOIN tmp2 ON expenses.id = tmp2.id
+--     SET expenses.totalPrice = tmp2.totalPrice;
 
--- tmp2のidがNULLのexpensesのtotalPriceを0にする
-UPDATE expenses
-LEFT JOIN tmp2 ON expenses.id = tmp2.id
-SET
-	expenses.totalPrice = 0
-WHERE
-	tmp2.id IS NULL;
+--     UPDATE expenses
+--     LEFT JOIN tmp2 ON expenses.id = tmp2.id
+--     SET expenses.totalPrice = 0
+--     WHERE tmp2.id IS NULL;
 
--- テンポラリテーブル削除
-DROP TEMPORARY TABLE tmp,
-tmp2;
+--     -- テンポラリテーブル削除
+--     DROP TEMPORARY TABLE tmp, tmp2;
+-- END;
 
-END;
-
-/ /
--- 終端文字戻す 
-DELIMITER;
