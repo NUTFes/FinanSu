c69626e84f68988b1ff17ada096f5ac5f57aa8e4
hikahana <22.h.hanada.nutfes@gmail.com>
2025-05-13 00:45:14 +0900

    feat: クエリ返り値用の型定義

---
+++ api/internals/domain/campus_donation.go
@@ -0,0 +1,14 @@
+package domain
+
+// CampusDonationRecord は、１行分のフラットな寄付データを表します
+type CampusDonationRecord struct {
+	BuildingId   int     `json:"building_id"`
+	BuildingName *string `json:"building_name,omitempty"`
+	FloorId      *int    `json:"floor_id,omitempty"`
+	FloorNumber  string  `json:"floor_number"`
+	TeacherId    int     `json:"teacher_id"`
+	TeacherName  string  `json:"teacher_name"`
+	RoomName     string  `json:"room_name"`
+	Price        int     `json:"price"`
+	IsBlack      *bool   `json:"is_black,omitempty"`
+}
