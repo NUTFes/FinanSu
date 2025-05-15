d4af303f5aa12053fbca3cd0f354e8755fc1983d
hikahana <22.h.hanada.nutfes@gmail.com>
2025-05-16 04:12:31 +0900

    [fix]: フラグを削除し、year_idを追加

--- mysql/db/20_campus_donations.sql
+++ mysql/db/20_campus_donations.sql
@@ -1,22 +1,18 @@
 use finansu_db;
 
-CREATE TABLE
-  campus_donations (
-    id int(10) unsigned not null auto_increment,
-    user_id int(10) not null,
-    teacher_id int(10) not null,
-    price int(10) not null,
-    remark varchar(255),
-    is_first_check boolean,
-    is_last_check boolean,
-    received_at varchar(255) not null,
-    created_at datetime not null default current_timestamp,
-    updated_at datetime not null default current_timestamp on update current_timestamp,
-    PRIMARY KEY (id)
-  );
+CREATE TABLE campus_donations (
+  id int(10) unsigned not null auto_increment,
+  user_id int(10) not null,
+  teacher_id int(10) not null,
+  price int(10) not null,
+  remark varchar(255),
+  received_at varchar(255) not null,
+  year_id INT(10) UNSIGNED NOT NULL,
+  created_at datetime not null default current_timestamp,
+  updated_at datetime not null default current_timestamp on update current_timestamp,
+  PRIMARY KEY (id)
+);
 
-INSERT INTO
-  campus_donations (user_id, teacher_id, price, remark, is_first_check, is_last_check, received_at)
-VALUES
-  (1, 1, 2000, "nothing", false, false, '2023-02-22'),
-  (2, 2, 2000, "nothing", false, false, '2022-02-22');
+INSERT INTO campus_donations (user_id, teacher_id, price, remark, received_at, year_id)
+VALUES (1, 1, 2000, "nothing", '2023-02-22', 2023),
+       (2, 2, 2000, "nothing", '2022-02-22', 2022);
