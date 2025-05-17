c7fd953b55ae9293efb8b2885305e4e108fe0201
TkymHrt <23.h.takayama.nutfes@gmail.com>
2025-05-17 14:05:45 +0900

    [fix] CSSのコンフリクトを解消

--- view/next-project/src/components/common/Tooltip/Tooltip.tsx
+++ view/next-project/src/components/common/Tooltip/Tooltip.tsx
@@ -9,7 +9,7 @@ function Tooltip(props: Props): JSX.Element {
   return (
     <div className='group relative flex flex-col items-center'>
       {props.children}
-      <div className='absolute bottom-0 mb-6 flex hidden flex-col items-center group-hover:flex'>
+      <div className='absolute bottom-0 mb-6 hidden flex-col items-center group-hover:flex'>
         <span className='whitespace-no-wrap relative z-10 rounded-md bg-black-600 p-2 text-xs leading-none text-white-0 shadow-lg'>
           {props.text}
         </span>
