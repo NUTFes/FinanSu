72f01f11b811283a2fcf2cd276c79aa2e34a19e8
nakatashingo <235711nakatashingo@gmail.com>
2025-12-29 00:18:31 +0900

    [otr] Simplify JSX formatting in PurchaseReportSummaryAmounts component

--- view/next-project/src/components/purchasereports/PurchaseReportSummaryAmounts.tsx
+++ view/next-project/src/components/purchasereports/PurchaseReportSummaryAmounts.tsx
@@ -16,14 +16,10 @@ export default function PurchaseReportSummaryAmounts({
       <div className='inline-grid grid-cols-[auto_auto_auto] gap-x-1 gap-y-1'>
         <span className='whitespace-nowrap'>未清算金額</span>
         <span className='whitespace-nowrap'>：</span>
-        <span className='min-w-[12ch] whitespace-nowrap text-right'>
-          {unsettledAmountText} 円
-        </span>
+        <span className='min-w-[12ch] whitespace-nowrap text-right'>{unsettledAmountText} 円</span>
         <span className='whitespace-nowrap'>未封詰め金額</span>
         <span className='whitespace-nowrap'>：</span>
-        <span className='min-w-[12ch] whitespace-nowrap text-right'>
-          {unpackedAmountText} 円
-        </span>
+        <span className='min-w-[12ch] whitespace-nowrap text-right'>{unpackedAmountText} 円</span>
       </div>
     </div>
   );
