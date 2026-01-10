29dda351916634016f8c8970e834bf3b1ec69353
Kubosaka <s223301@stn.nagaokaut.ac.jp>
2026-01-10 23:59:28 +0900

    fix

--- api/externals/handler/wire.go
+++ api/externals/handler/wire.go
@@ -1,40 +1,13 @@
 //go:build wireinject
 // +build wireinject
 
-package controller
+package handler
 
 import (
 	"github.com/google/wire"
 )
 
-// ControllerProviderSet - Controller層のProviderセット
-var ControllerProviderSet = wire.NewSet(
-	NewActivityController,
-	NewActivityInformationController,
-	NewActivityStyleController,
-	NewBudgetController,
-	NewBureauController,
-	NewBuyReportController,
-	NewDepartmentController,
-	NewDivisionController,
-	NewExpenseController,
-	NewFestivalItemController,
-	NewFinancialRecordController,
-	NewFundInformationController,
-	NewHealthCheckController,
-	NewIncomeController,
-	NewIncomeExpenditureManagementController,
-	NewMailAuthController,
-	NewObjectUploadController,
-	NewPasswordResetTokenController,
-	NewPurchaseItemController,
-	NewPurchaseOrderController,
-	NewPurchaseReportController,
-	NewReceiptController,
-	NewSourceController,
-	NewSponsorController,
-	NewSponsorStyleController,
-	NewTeacherController,
-	NewUserController,
-	NewYearController,
+// HandlerProviderSet - Handler層のProviderセット
+var HandlerProviderSet = wire.NewSet(
+	NewHandler,
 )
--- api/internals/di/wire.go
+++ api/internals/di/wire.go
@@ -7,7 +7,7 @@ import (
 	"github.com/NUTFes/FinanSu/api/drivers/db"
 	"github.com/NUTFes/FinanSu/api/drivers/mc"
 	"github.com/NUTFes/FinanSu/api/drivers/server"
-	"github.com/NUTFes/FinanSu/api/externals/controller"
+	"github.com/NUTFes/FinanSu/api/externals/handler"
 	"github.com/NUTFes/FinanSu/api/externals/repository"
 	"github.com/NUTFes/FinanSu/api/externals/repository/abstract"
 	"github.com/NUTFes/FinanSu/api/internals/usecase"
@@ -41,64 +41,10 @@ func ProvideCrud(client db.Client) abstract.Crud {
 
 // ProvideRouter - Router構築のProvider
 func ProvideRouter(
-	activityController controller.ActivityController,
-	activityInformationController controller.ActivityInformationController,
-	activityStyleController controller.ActivityStyleController,
-	budgetController controller.BudgetController,
-	bureauController controller.BureauController,
-	buyReportController controller.BuyReportController,
-	departmentController controller.DepartmentController,
-	divisionController controller.DivisionController,
-	expenseController controller.ExpenseController,
-	festivalItemController controller.FestivalItemController,
-	financialRecordController controller.FinancialRecordController,
-	fundInformationController controller.FundInformationController,
-	healthcheckController controller.HealthcheckController,
-	incomeController controller.IncomeController,
-	incomeExpenditureManagementController controller.IncomeExpenditureManagementController,
-	mailAuthController controller.MailAuthController,
-	objectUploadController controller.ObjectUploadController,
-	passwordResetTokenController controller.PasswordResetTokenController,
-	purchaseItemController controller.PurchaseItemController,
-	purchaseOrderController controller.PurchaseOrderController,
-	purchaseReportController controller.PurchaseReportController,
-	receiptController controller.ReceiptController,
-	sourceController controller.SourceController,
-	sponsorController controller.SponsorController,
-	sponsorStyleController controller.SponsorStyleController,
-	teacherController controller.TeacherController,
-	userController controller.UserController,
-	yearController controller.YearController,
+	handler handler.Handler,
 ) router.Router {
 	return router.NewRouter(
-		activityController,
-		activityInformationController,
-		activityStyleController,
-		budgetController,
-		bureauController,
-		buyReportController,
-		departmentController,
-		divisionController,
-		expenseController,
-		festivalItemController,
-		financialRecordController,
-		fundInformationController,
-		healthcheckController,
-		incomeController,
-		incomeExpenditureManagementController,
-		mailAuthController,
-		objectUploadController,
-		passwordResetTokenController,
-		purchaseItemController,
-		purchaseOrderController,
-		purchaseReportController,
-		receiptController,
-		sourceController,
-		sponsorController,
-		sponsorStyleController,
-		teacherController,
-		userController,
-		yearController,
+		handler
 	)
 }
 
@@ -126,7 +72,7 @@ func InitializeServer() (*ServerComponents, error) {
 		// 各層のProviderセット
 		repository.RepositoryProviderSet,
 		usecase.UseCaseProviderSet,
-		controller.ControllerProviderSet,
+		handler.HandlerProviderSet,
 
 		// Router/Server
 		ProvideRouter,
