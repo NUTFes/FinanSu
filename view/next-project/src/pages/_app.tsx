01034be1c35ea22c6f3def095d21f5f292aa5962
TkymHrt <23.h.takayama.nutfes@gmail.com>
2026-01-14 00:25:38 +0900

    fix: NotoSansフォントを再適応（next.js, Issue, #86792）

--- view/next-project/src/pages/_app.tsx
+++ view/next-project/src/pages/_app.tsx
@@ -1,3 +1,4 @@
+import { Noto_Sans_JP } from 'next/font/google';
 import Head from 'next/head';
 import { NuqsAdapter } from 'nuqs/adapters/next/pages';
 
@@ -9,19 +10,27 @@ import type { AppProps } from 'next/app';
 
 import '@/styles/globals.css';
 
+const notoSansJP = Noto_Sans_JP({
+  subsets: ['latin'],
+  display: 'swap',
+  variable: '--font-noto-sans-jp',
+});
+
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ToastProvider>
       <Head>
         <link rel='icon' href='/favicon.ico' />
       </Head>
-      <ManagedUIContext>
-        <Layout>
-          <NuqsAdapter>
-            <Component {...pageProps} />
-          </NuqsAdapter>
-        </Layout>
-      </ManagedUIContext>
+      <main className={`${notoSansJP.className} ${notoSansJP.variable} h-full`}>
+        <ManagedUIContext>
+          <Layout>
+            <NuqsAdapter>
+              <Component {...pageProps} />
+            </NuqsAdapter>
+          </Layout>
+        </ManagedUIContext>
+      </main>
     </ToastProvider>
   );
 }
