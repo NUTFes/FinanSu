f616f3f02bac5f0bc983b1f6dc5cae2d4b8783f0
Wakai111 <209897712+Wakai111@users.noreply.github.com>
2026-03-07 15:22:50 +0000

    formatted by workflow

--- view/next-project/src/pages/reset_password/[id]/index.tsx
+++ view/next-project/src/pages/reset_password/[id]/index.tsx
@@ -161,9 +161,9 @@ export default function ResetPassword(props: Props) {
                         })}
                       />
                     </div>
-                    <div
-                      className='mb-5 flex flex-col items-center justify-center'
-                    >
+                    <div className='
+                      mb-5 flex flex-col items-center justify-center
+                    '>
                       <p className='text-red-500'>{errors.password && errors.password.message}</p>
                       <p className='text-red-500'>
                         {errors.confirmPassword && errors.confirmPassword.type === 'correct' && (
--- view/next-project/src/pages/yearperiods/index.tsx
+++ view/next-project/src/pages/yearperiods/index.tsx
@@ -114,9 +114,9 @@ export default function Periods(props: Props) {
                       className={clsx(
                         'px-1 py-3',
                         index === 0 ? 'pt-4 pb-3' : 'py-3',
-                        index === formatYearPeriods.length - 1
-                          ? 'pt-3 pb-4'
-                          : `border-b py-3`,
+                        index === formatYearPeriods.length - 1 ? 'pt-3 pb-4' : `
+                          border-b py-3
+                        `,
                       )}
                     >
                       <p className='text-center text-sm text-black-600'>{yearPeriod.id}</p>
@@ -125,9 +125,9 @@ export default function Periods(props: Props) {
                       className={clsx(
                         'px-1',
                         index === 0 ? 'pt-4 pb-3' : 'py-3',
-                        index === formatYearPeriods.length - 1
-                          ? 'pt-3 pb-4'
-                          : `border-b py-3`,
+                        index === formatYearPeriods.length - 1 ? 'pt-3 pb-4' : `
+                          border-b py-3
+                        `,
                       )}
                     >
                       <p className='text-center text-sm text-black-600'>{yearPeriod.year}</p>
@@ -136,9 +136,9 @@ export default function Periods(props: Props) {
                       className={clsx(
                         'px-1',
                         index === 0 ? 'pt-4 pb-3' : 'py-3',
-                        index === formatYearPeriods.length - 1
-                          ? 'pt-3 pb-4'
-                          : `border-b py-3`,
+                        index === formatYearPeriods.length - 1 ? 'pt-3 pb-4' : `
+                          border-b py-3
+                        `,
                       )}
                     >
                       <p className='text-center text-sm text-black-600'>{yearPeriod.startedAt}</p>
@@ -147,9 +147,9 @@ export default function Periods(props: Props) {
                       className={clsx(
                         'px-1',
                         index === 0 ? 'pt-4 pb-3' : 'py-3',
-                        index === formatYearPeriods.length - 1
-                          ? 'pt-3 pb-4'
-                          : `border-b py-3`,
+                        index === formatYearPeriods.length - 1 ? 'pt-3 pb-4' : `
+                          border-b py-3
+                        `,
                       )}
                     >
                       <p className='text-center text-sm text-black-600'>{yearPeriod.endedAt}</p>
@@ -158,9 +158,9 @@ export default function Periods(props: Props) {
                       className={clsx(
                         'px-1',
                         index === 0 ? 'pt-4 pb-3' : 'py-3',
-                        index === formatYearPeriods.length - 1
-                          ? 'pt-3 pb-4'
-                          : `border-b py-3`,
+                        index === formatYearPeriods.length - 1 ? 'pt-3 pb-4' : `
+                          border-b py-3
+                        `,
                       )}
                     >
                       <div className='flex gap-2'>
