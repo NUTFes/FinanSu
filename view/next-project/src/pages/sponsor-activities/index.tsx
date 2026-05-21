631bd4d0e10eb6e5a4487c3fd39e197b0b8b3d9c
TkymHrt <23.h.takayama.nutfes@gmail.com>
2026-05-21 22:25:58 +0900

    fix: 権限の調整

--- view/next-project/src/pages/sponsor-activities/index.tsx
+++ view/next-project/src/pages/sponsor-activities/index.tsx
@@ -90,7 +90,7 @@ export default function SponsorActivities(props: Props) {
       router.push('/');
       return;
     }
-    if (user.roleID !== 2 && user.roleID !== 4) {
+    if (user.roleID !== 2 && user.roleID !== 3 && user.roleID !== 4) {
       router.push('/my_page');
     }
   }, [_hasHydrated, user?.roleID, router]);
@@ -184,7 +184,7 @@ export default function SponsorActivities(props: Props) {
   }, [filterData, sponsorIdSetByYear]);
 
   if (!_hasHydrated) return <Loading />;
-  if (!user?.roleID || (user.roleID !== 2 && user.roleID !== 4)) return <Loading />;
+  if (!user?.roleID || (user.roleID !== 2 && user.roleID !== 3 && user.roleID !== 4)) return <Loading />;
 
   return (
     <SponsorActivitiesLayout
