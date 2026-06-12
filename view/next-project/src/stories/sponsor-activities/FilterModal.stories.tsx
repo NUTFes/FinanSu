2e47650b1043b2f036b1fd48c7187629e72935e5
wakai111 <25.y.wakai.nutfes@gmail.com>
2026-06-13 01:00:43 +0900

    fix: FilterModal.stories.tsxから使っていない変数の削除

--- view/next-project/src/stories/sponsor-activities/FilterModal.stories.tsx
+++ view/next-project/src/stories/sponsor-activities/FilterModal.stories.tsx
@@ -1,7 +1,7 @@
 import { SponsorActivitiesFilterType } from '@/utils/sponsorshipActivity';
 import { FilterModal } from '@components/sponsor-activities';
 
-import { SPONSOR, SPONSOR_STYLE, USER } from '../constants';
+import { SPONSOR_STYLE, USER } from '../constants';
 
 import type { Meta, StoryFn } from '@storybook/react';
 
@@ -31,7 +31,6 @@ Primary.args = {
   setIsOpen: () => {},
   sponsorStyles: [SPONSOR_STYLE],
   users: [USER],
-  sponsors: [SPONSOR],
   filterData,
   setFilterData: () => {},
 };
